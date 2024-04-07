c8f797d686edf33acefd3807da79e8effb63fcc3
HayHay <dev@hayhay.dev>
2024-04-07 01:58:44 -0400

    Refactor assignments list and sliver assignments list widgets

---
+++ lib/assignments/widgets/assignments_list.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+enum AssignmentsFilter { all, starred, completed }
+
+class AssignmentsList extends StatelessWidget {
+  const AssignmentsList(
+      {super.key, required this.assignments, required this.filter});
+
+  final List<Map<String, dynamic>> assignments;
+  final AssignmentsFilter filter;
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverList(
+      delegate: SliverChildBuilderDelegate(
+        (BuildContext context, int index) {
+          if (filter == AssignmentsFilter.starred &&
+              !assignments[index]['starred']) {
+            return const SizedBox.shrink();
+          }
+
+          if (filter == AssignmentsFilter.completed &&
+              !assignments[index]['completed']) {
+            return const SizedBox.shrink();
+          }
+
+          return Card(
+            child: Container(
+              padding: const EdgeInsets.all(16.0),
+              margin: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    assignments[index]['title'],
+                    style: const TextStyle(
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 8.0),
+                  Text(
+                    assignments[index]['description'],
+                    style: const TextStyle(fontSize: 16.0),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+        childCount: assignments.length,
+      ),
+    );
+  }
+}
---
+++ lib/assignments/widgets/sliver_assignments_list.dart
@@ -0,0 +1,58 @@
+import 'package:assigngo_rewrite/assignments/widgets/assignments_list.dart';
+import 'package:flutter/material.dart';
+
+class SliverAssignmentsList extends StatelessWidget {
+  const SliverAssignmentsList(
+      {super.key,
+      required this.assignments,
+      required this.filter,
+      required this.colors,
+      required this.title});
+
+  final List<Map<String, dynamic>> assignments;
+  final AssignmentsFilter filter;
+  final List<Color> colors;
+  final String title;
+
+  final bool _pinned = true;
+  final bool _snap = false;
+  final bool _floating = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomScrollView(shrinkWrap: true, slivers: [
+      SliverAppBar(
+        pinned: _pinned,
+        snap: _snap,
+        floating: _floating,
+        expandedHeight: 150.0,
+        backgroundColor: Colors.deepPurple,
+        flexibleSpace: FlexibleSpaceBar(
+          collapseMode: CollapseMode.pin,
+          background: Opacity(
+            opacity: 0.5,
+            child: Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: colors,
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                ),
+              ),
+            ),
+          ),
+          title: Align(
+            alignment: Alignment.bottomLeft,
+            child: Text(
+              title,
+            ),
+          ),
+        ),
+      ),
+      SliverPadding(
+        padding: const EdgeInsets.all(8.0),
+        sliver: AssignmentsList(assignments: assignments, filter: filter),
+      ),
+    ]);
+  }
+}
