b5d847aa617483fa3a045168c3a3ab6df21ea506
HayHay <dev@hayhay.dev>
2024-04-07 22:36:15 -0400

    Initial AssignmentCard widget with Dismissible for assignment actions

---
+++ lib/assignments/assignment/ui/assignment_card.dart
@@ -0,0 +1,107 @@
+import 'package:assigngo_rewrite/assignments/models/assignments_model.dart';
+import 'package:assigngo_rewrite/assignments/providers/assignments_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class AssignmentCard extends ConsumerStatefulWidget {
+  const AssignmentCard({
+    super.key,
+    required this.assignment,
+  });
+
+  final Assignment assignment;
+
+  @override
+  ConsumerState<AssignmentCard> createState() => _AssignmentCardState();
+}
+
+class _AssignmentCardState extends ConsumerState<AssignmentCard> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final Assignment assignment = widget.assignment;
+    return Dismissible(
+      key: ValueKey(widget.assignment.id),
+      confirmDismiss: (direction) {
+        if (direction == DismissDirection.startToEnd) {
+          ref
+              .read(assignmentsProvider.notifier)
+              .starAssignment(widget.assignment.id!);
+          return Future.value(true);
+        } else {
+          ref
+              .read(assignmentsProvider.notifier)
+              .completeAssignment(widget.assignment.id!);
+          return Future.value(false);
+        }
+      },
+      background: Container(
+        color: Colors.amber,
+        alignment: Alignment.centerLeft,
+        padding: const EdgeInsets.only(left: 20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(assignment.starred ? Icons.star : Icons.star_outline,
+                color: Colors.black, size: 32.0),
+            const Text(
+              "Star",
+              style: TextStyle(color: Colors.black, fontSize: 16.0),
+            ),
+          ],
+        ),
+      ),
+      secondaryBackground: Container(
+        color: assignment.completed ? Colors.red : Colors.green,
+        alignment: Alignment.centerRight,
+        padding: const EdgeInsets.only(right: 20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(assignment.completed ? Icons.cancel_presentation : Icons.check,
+                color: Colors.black, size: 32.0),
+            Text(
+              assignment.completed ? "Incomplete" : "Complete",
+              style: const TextStyle(color: Colors.black, fontSize: 16.0),
+            ),
+          ],
+        ),
+      ),
+      child: SizedBox(
+        height: 120.0,
+        width: double.infinity,
+        child: Card(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(16.0),
+          ),
+          elevation: 2.0,
+          child: Container(
+            padding: const EdgeInsets.all(16.0),
+            margin: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  widget.assignment.title,
+                  style: const TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 8.0),
+                Text(
+                  widget.assignment.description ?? "",
+                  style: const TextStyle(fontSize: 16.0),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
