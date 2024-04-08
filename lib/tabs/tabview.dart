0b5185001a63f00a2d9910c4952be616c1376482
HayHay <dev@hayhay.dev>
2024-04-08 00:45:47 -0400

    Refactor tabview.dart to support mobile and desktop layouts

--- lib/tabs/tabview.dart
+++ lib/tabs/tabview.dart
@@ -1,3 +1,4 @@
+import 'package:assigngo_rewrite/assignments/assignment/views/assignment_screen.dart';
 import 'package:assigngo_rewrite/assignments/providers/assignments_provider.dart';
 import 'package:assigngo_rewrite/assignments/views/home_screen.dart';
 import 'package:assigngo_rewrite/assignments/views/priority_screen.dart';
@@ -51,34 +52,96 @@ class _TabViewState extends ConsumerState<TabView> {
       // Settings(),
     ];
 
-    return Scaffold(
-      body: pages[_selectedIndex],
-      persistentFooterButtons: [
-        ElevatedButton(
-          onPressed: () {
-            // Get.to(() => SearchModal());
-          },
-          child: const Row(
-            children: [
-              Icon(Icons.search),
-              Text("Search"),
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        if (MediaQuery.of(context).size.width < 800) {
+          // Mobile layout
+          return Scaffold(
+            body: pages[_selectedIndex],
+            persistentFooterButtons: [
+              ElevatedButton(
+                onPressed: () {},
+                child: const Row(
+                  children: [
+                    Icon(Icons.search),
+                    Text("Search"),
+                  ],
+                ),
+              ),
             ],
-          ),
-        ),
-      ],
-      bottomNavigationBar: NavigationBar(
-          onDestinationSelected: _onItemTapped,
-          selectedIndex: _selectedIndex,
-          indicatorColor: Colors.purple,
-          destinations: const [
-            NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
-            NavigationDestination(icon: Icon(Icons.star), label: "Prioritized"),
-            NavigationDestination(icon: Icon(Icons.add), label: "Add"),
-            NavigationDestination(
-                icon: Icon(Icons.check_box), label: "Completed"),
-            NavigationDestination(
-                icon: Icon(Icons.settings), label: "Settings"),
-          ]),
+            bottomNavigationBar: NavigationBar(
+              onDestinationSelected: _onItemTapped,
+              selectedIndex: _selectedIndex,
+              indicatorColor: Colors.purple,
+              destinations: const [
+                NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
+                NavigationDestination(
+                    icon: Icon(Icons.star), label: "Prioritized"),
+                NavigationDestination(icon: Icon(Icons.add), label: "Add"),
+                NavigationDestination(
+                  icon: Icon(Icons.check_box),
+                  label: "Completed",
+                ),
+                NavigationDestination(
+                  icon: Icon(Icons.settings),
+                  label: "Settings",
+                ),
+              ],
+            ),
+          );
+        } else {
+          // Desktop layout
+          return Scaffold(
+            body: Row(
+              children: [
+                NavigationRail(
+                  selectedIndex: _selectedIndex,
+                  onDestinationSelected: _onItemTapped,
+                  labelType: NavigationRailLabelType.all,
+                  destinations: const [
+                    NavigationRailDestination(
+                      icon: Icon(Icons.home),
+                      label: Text('Home'),
+                    ),
+                    NavigationRailDestination(
+                      icon: Icon(Icons.star),
+                      label: Text("Prioritized"),
+                    ),
+                    NavigationRailDestination(
+                      icon: Icon(Icons.add),
+                      label: Text("Add"),
+                    ),
+                    NavigationRailDestination(
+                      icon: Icon(Icons.check_box),
+                      label: Text("Completed"),
+                    ),
+                    NavigationRailDestination(
+                      icon: Icon(Icons.settings),
+                      label: Text("Settings"),
+                    ),
+                  ],
+                ),
+                Expanded(
+                  flex: 3,
+                  child: pages[_selectedIndex],
+                ),
+                const Expanded(flex: 2, child: AssignmentScreen()),
+              ],
+            ),
+            persistentFooterButtons: [
+              ElevatedButton(
+                onPressed: () {},
+                child: const Row(
+                  children: [
+                    Icon(Icons.search),
+                    Text("Search"),
+                  ],
+                ),
+              ),
+            ],
+          );
+        }
+      },
     );
   }
 }
