5f5a17d135b0585f6fbbc398ee462900f2ee75d8
HayHay <dev@hayhay.dev>
2024-04-13 21:36:30 -0400

    feat: Add Supabase constants for API URL and key

---
+++ lib/constants.dart
@@ -0,0 +1,3 @@
+const supabaseUrl = 'https://mzudaknbrzixjkvjqayw.supabase.co';
+const supabaseKey =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im16dWRha25icnppeGprdmpxYXl3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTI0MjU3NDgsImV4cCI6MjAyODAwMTc0OH0.b71_fWtic8S4sfNmCMlwLAlzZwhS_lHGBEW1ZQynfsc";
