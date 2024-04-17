b90534690052c5ecf87eff72b19468e78f67acfe
HayHay <dev@hayhay.dev>
2024-04-17 18:30:48 -0400

    Refactor subtask provider & repo for appwrite

--- lib/assignments/subtasks/providers/subtasks_providers.dart
+++ lib/assignments/subtasks/providers/subtasks_providers.dart
@@ -1,6 +1,5 @@
 import 'package:assigngo_rewrite/assignments/subtasks/models/subtasks_model/subtasks_model.dart';
 import 'package:assigngo_rewrite/assignments/subtasks/repositories/subtasks_repository.dart';
-import 'package:assigngo_rewrite/shared/models/return_model/return_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -11,31 +10,10 @@ class SubtasksNotifier extends ChangeNotifier {
 
   List<Subtask> subtasks = [];
 
-  Future<void> getAssignmentSubtasks(int assignmentId) async {
-    subtasks = await _subtasksRepository.getAssignmentSubtasks(assignmentId);
-    notifyListeners();
-  }
-
-  Future<void> createSubtask(Subtask subtask) async {
-    final returnModel = await _subtasksRepository.createSubtask(subtask);
-    if (returnModel.success) {
-      subtasks.add(subtask);
-    }
-  }
-
-  Future<ReturnModel> createBatchSubtasks(List<Subtask> subtasks) async {
-    final returnModel = await _subtasksRepository.createBatchSubtasks(subtasks);
-    if (returnModel.success) {
-      this.subtasks.addAll(subtasks);
-      notifyListeners();
-    }
-    return returnModel;
-  }
-
   Future<void> toggleSubtaskDone(Subtask subtask) async {
-    final returnModel = await _subtasksRepository.toggleComplete(subtask);
+    final returnModel = await _subtasksRepository.updateSubtask(subtask);
     if (returnModel.success) {
-      int index = subtasks.indexWhere((element) => element.id == subtask.id);
+      int index = subtasks.indexWhere((element) => element.$id == subtask.$id);
 
       Subtask updatedSubtask = subtask.copyWith(completed: !subtask.completed);
 
--- lib/assignments/subtasks/repositories/subtasks_repository.dart
+++ lib/assignments/subtasks/repositories/subtasks_repository.dart
@@ -1,72 +1,24 @@
 import 'package:assigngo_rewrite/assignments/subtasks/models/subtasks_model/subtasks_model.dart';
+import 'package:assigngo_rewrite/constants.dart';
 import 'package:assigngo_rewrite/shared/models/return_model/return_model.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
 
 class SubtasksRepository {
-  final SupabaseClient supabase = Supabase.instance.client;
-
   SubtasksRepository();
 
-  Future<List<Subtask>> getAssignmentSubtasks(int assignmentId) async {
-    final subtask = await supabase
-        .from('subtasks')
-        .select()
-        .eq('assignmentId', assignmentId)
-        .order('priority', ascending: true);
-
-    return subtask.map((e) => Subtask.fromJson(e)).toList();
-  }
-
-  Future<ReturnModel> createBatchSubtasks(List<Subtask> subtasks) async {
-    final List<Map<String, dynamic>> jsonSubtasks =
-        subtasks.map((e) => e.toJson()).toList();
-
-    for (var element in jsonSubtasks) {
-      element.remove('id');
-      element.remove('createdAt');
-      element.remove('updatedAt');
-    }
-
+  Future<ReturnModel> updateSubtask(Subtask subtask) async {
     try {
-      await supabase.from('subtasks').insert(jsonSubtasks);
-      return const ReturnModel(
-          message: "Subtasks created successfully", success: true);
-    } catch (error) {
-      return ReturnModel(
-          message: "Error creating subtasks",
-          success: false,
-          error: error.toString());
-    }
-  }
+      final updatedSubtask = await database.updateDocument(
+          databaseId: publicDb,
+          collectionId: "subtasks",
+          documentId: subtask.$id,
+          data: subtask.toJson());
 
-  Future<ReturnModel> createSubtask(Subtask subtask) async {
-    final Map<String, dynamic> jsonSubtask = subtask.toJson();
-    jsonSubtask.remove('id');
-
-    try {
-      await supabase.from('subtasks').insert(jsonSubtask);
-      return const ReturnModel(
-          message: "Subtask created successfully", success: true);
-    } catch (error) {
       return ReturnModel(
-          message: "Error creating subtask",
-          success: false,
-          error: error.toString());
-    }
-  }
-
-  Future<ReturnModel> toggleComplete(Subtask subtask) async {
-    try {
-      await supabase
-          .from('subtasks')
-          .update({'completed': !subtask.completed}).eq('id', subtask.id!);
-      return const ReturnModel(
-          message: "Subtask completed successfully", success: true);
+          success: true,
+          data: updatedSubtask.data,
+          message: "Subtask updated successfully");
     } catch (error) {
-      return ReturnModel(
-          message: "Error completing subtask",
-          success: false,
-          error: error.toString());
+      return ReturnModel(success: false, message: error.toString());
     }
   }
 }
