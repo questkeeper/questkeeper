56c73115a69b2a7a2f0deeed8d0665ab427dfc75
HayHay <dev@hayhay.dev>
2024-04-27 18:06:44 -0400

    Abstract date time picker to shared widget

---
+++ lib/assignments/widgets/date_time_picker.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+Future<void> showDateTimePicker(BuildContext context, DateTime dueDate,
+    Function(DateTime) onDateTimeSelected) async {
+  await showDatePicker(
+    context: context,
+    initialDate: dueDate,
+    lastDate: DateTime(2100),
+    firstDate: dueDate.isBefore(DateTime.now()) ? dueDate : DateTime.now(),
+  ).then((selectedDate) async {
+    // After selecting the date, display the time picker.
+    if (selectedDate != null) {
+      await showTimePicker(
+        context: context,
+        initialTime: dueDate == selectedDate
+            ? TimeOfDay.fromDateTime(dueDate)
+            : const TimeOfDay(hour: 23, minute: 59),
+      ).then((selectedTime) {
+        // Handle the selected date and time here.
+        if (selectedTime != null) {
+          DateTime selectedDateTime = DateTime(
+            selectedDate.year,
+            selectedDate.month,
+            selectedDate.day,
+            selectedTime.hour,
+            selectedTime.minute,
+          );
+
+          onDateTimeSelected(selectedDateTime);
+        }
+      });
+    }
+  });
+}
