45dbdc6e5b921fe9d7c4390f7374a5908d7be5a8
HayHay <dev@hayhay.dev>
2024-04-29 10:11:18 -0400

    feat: Update UI to show ability to edit assignments

--- lib/assignments/assignment/views/assignment_screen.dart
+++ lib/assignments/assignment/views/assignment_screen.dart
@@ -2,8 +2,11 @@ import 'package:assigngo_rewrite/assignments/models/assignments_model.dart';
 import 'package:assigngo_rewrite/assignments/providers/current_assignment_provider.dart';
 import 'package:assigngo_rewrite/assignments/providers/assignments_provider.dart';
 import 'package:assigngo_rewrite/assignments/subtasks/models/subtasks_model/subtasks_model.dart';
-import 'package:assigngo_rewrite/assignments/subtasks/providers/subtasks_providers.dart';
+import 'package:assigngo_rewrite/assignments/widgets/date_time_picker.dart';
+import 'package:assigngo_rewrite/assignments/widgets/subject_dropdown_field.dart';
 import 'package:assigngo_rewrite/shared/utils/format_date.dart';
+import 'package:assigngo_rewrite/shared/widgets/snackbar.dart';
+import 'package:assigngo_rewrite/subjects/providers/subjects_provider.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -21,6 +24,13 @@ class _AssignmentScreenState extends ConsumerState<AssignmentScreen> {
     ref.read(assignmentsProvider.notifier).updateAssignment(assignment);
   }
 
+  void _updateAssignmentSubject(Assignment assignment) {
+    ref
+        .read(currentAssignmentProvider.notifier)
+        .updateAssignmentSubject(assignment);
+    ref.read(assignmentsProvider.notifier).updateAssignmentSubject(assignment);
+  }
+
   void _deleteAssignment(Assignment assignment) {
     ref.read(assignmentsProvider.notifier).deleteAssignment(assignment);
     ref.read(currentAssignmentProvider.notifier).setCurrentAssignment(null);
@@ -30,13 +40,44 @@ class _AssignmentScreenState extends ConsumerState<AssignmentScreen> {
   void _subtaskComplete(Subtask subtask) {
     subtask = subtask.copyWith(completed: !subtask.completed);
     ref.read(currentAssignmentProvider.notifier).updateSubtask(subtask);
-    ref.read(subtasksProvider.notifier).toggleSubtaskDone(subtask);
+    ref
+        .read(assignmentsProvider.notifier)
+        .updateAssignment(ref.read(currentAssignmentProvider).assignment!);
   }
+
+  void _updateSubtask(Subtask subtask) {
+    ref.read(currentAssignmentProvider.notifier).updateSubtask(subtask);
+    ref
+        .read(assignmentsProvider.notifier)
+        .updateAssignment(ref.read(currentAssignmentProvider).assignment!);
+  }
+
+  void _createSubtask(Assignment assignment) async {
+    await ref.read(assignmentsProvider.notifier).createSubtask(assignment);
+    ref
+        .read(currentAssignmentProvider.notifier)
+        .updateCurrentAssignment(assignment);
+  }
+
+  bool readOnly = true;
 
   @override
   Widget build(BuildContext context) {
+    void setReadOnly(bool value) {
+      SnackbarService.showInfoSnackbar(
+          context,
+          !value
+              ? "Tap a field to edit it"
+              : "You are no longer editing the assignment");
+
+      setState(() {
+        readOnly = value;
+      });
+    }
+
     final currentAssignmentRef =
         ref.watch(currentAssignmentProvider).assignment;
+    final updatedAssignment = ref.watch(currentAssignmentProvider).assignment;
 
     final platformBrightness = MediaQuery.of(context).platformBrightness;
     final size = MediaQuery.of(context).size;
@@ -57,7 +98,8 @@ class _AssignmentScreenState extends ConsumerState<AssignmentScreen> {
       );
     }
 
-    final currentAssignment = currentAssignmentRef;
+    final currentAssignment = updatedAssignment ?? currentAssignmentRef;
+    final subjectsList = ref.watch(subjectsProvider);
 
     // Local variables to store the current assignment's categories, starred, and completed status
     Set<Categories> categories = currentAssignment.categories.toSet();
@@ -93,6 +135,8 @@ class _AssignmentScreenState extends ConsumerState<AssignmentScreen> {
                           .read(currentAssignmentProvider.notifier)
                           .setCurrentAssignment(null);
                     },
+                    setReadOnly: setReadOnly,
+                    readOnly: readOnly,
                   ),
 
             // Use the enum to create chips
@@ -130,69 +174,168 @@ class _AssignmentScreenState extends ConsumerState<AssignmentScreen> {
               ),
             ),
             Container(
-              padding: const EdgeInsets.all(8.0),
+              padding: const EdgeInsets.fromLTRB(8.0, 8.0, 0, 0),
               alignment: Alignment.centerLeft,
-              child: Text(
-                  style: Theme.of(context).textTheme.headlineLarge,
-                  currentAssignment.title),
-            ),
-            Container(
-              padding: const EdgeInsets.all(8.0),
-              alignment: Alignment.centerLeft,
-              child: Text(
-                currentAssignment.description ?? "",
-                style: Theme.of(context).textTheme.bodyMedium,
+              child: TextField(
+                readOnly: readOnly,
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  hintText: currentAssignment.title,
+                  hintStyle: Theme.of(context).textTheme.headlineLarge,
+                ),
+                style: Theme.of(context).textTheme.headlineLarge,
+                onSubmitted: (String value) {
+                  final Assignment updatedAssignment =
+                      currentAssignment.copyWith(title: value);
+                  _updateAssignment(updatedAssignment);
+                },
               ),
             ),
             Container(
-              padding: const EdgeInsets.all(8.0),
+              padding: const EdgeInsets.only(left: 8.0),
               alignment: Alignment.centerLeft,
-              child: Text(
-                "Due ${formatDate(currentAssignment.dueDate)}",
-                style: Theme.of(context).textTheme.bodySmall,
+              child: readOnly == true
+                  ? Text(
+                      currentAssignment.subject?.name ?? "Select a subject",
+                      style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                            color: Colors.grey,
+                          ),
+                    )
+                  : SubjectDropdownField(
+                      subjectsList: subjectsList,
+                      onSubjectChanged: (id) {
+                        setState(() {
+                          late Assignment updatedAssignment;
+                          if (id == '-1' || id == null || id == '') {
+                            updatedAssignment =
+                                currentAssignment.copyWith(subject: null);
+                            debugPrint("subject: $updatedAssignment");
+                          } else {
+                            final subject = subjectsList
+                                .firstWhere((subject) => subject.$id == id);
+
+                            updatedAssignment =
+                                currentAssignment.copyWith(subject: subject);
+                            debugPrint("subject: $updatedAssignment");
+                          }
+                          _updateAssignmentSubject(updatedAssignment);
+                        });
+                      },
+                      defaultSubjectId: currentAssignment.subject?.$id,
+                    ),
+            ),
+
+            Container(
+              padding: const EdgeInsets.only(left: 8.0),
+              alignment: Alignment.centerLeft,
+              child: TextField(
+                readOnly: readOnly,
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  hintText: currentAssignment.description != null &&
+                          currentAssignment.description!.isNotEmpty
+                      ? currentAssignment.description
+                      : "Description here..",
+                  hintStyle: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                        color: Colors.grey,
+                      ),
+                ),
+                style: Theme.of(context).textTheme.bodyMedium,
+                onSubmitted: (String value) {
+                  final Assignment updatedAssignment =
+                      currentAssignment.copyWith(description: value);
+                  _updateAssignment(updatedAssignment);
+                },
+              ),
+            ),
+            InkWell(
+              onTap: () async {
+                if (readOnly == true) {
+                  return;
+                }
+                await showDateTimePicker(
+                  context,
+                  currentAssignment.dueDate,
+                  (DateTime selectedDateTime) {
+                    final Assignment updatedAssignment =
+                        currentAssignment.copyWith(dueDate: selectedDateTime);
+                    _updateAssignment(updatedAssignment);
+                  },
+                );
+              },
+              child: Container(
+                padding: const EdgeInsets.all(8.0),
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  "Due ${formatDate(currentAssignment.dueDate)}",
+                  style: Theme.of(context).textTheme.bodySmall,
+                ),
               ),
             ),
             const Divider(),
             Container(
               padding: const EdgeInsets.all(8.0),
               alignment: Alignment.centerLeft,
-              child: Text(
-                "Subtasks",
-                style: Theme.of(context).textTheme.headlineSmall,
+              child: Row(
+                children: [
+                  Text(
+                    "Subtasks",
+                    style: Theme.of(context).textTheme.headlineSmall,
+                  ),
+                  IconButton(
+                    icon: const Icon(Icons.add_box_outlined),
+                    onPressed: () {
+                      _createSubtask(currentAssignment);
+                    },
+                  ),
+                ],
               ),
             ),
-            ListView.builder(
-              shrinkWrap: true,
-              itemCount: subtasks?.length ?? 0,
-              itemBuilder: (context, index) {
-                final subtask = subtasks?[index];
-                return CheckboxListTile(
-                  subtitle: subtask?.priority != null
-                      ? Text(
-                          "Priority: ${subtask?.priority}",
-                          style: Theme.of(context)
-                              .textTheme
-                              .bodySmall
-                              ?.copyWith(color: Colors.amber),
-                        )
-                      : null,
-                  value: subtask?.completed,
-                  title: Text(
-                    subtask!.title,
-                    style: Theme.of(context).textTheme.bodyMedium,
-                  ),
-                  onChanged: (bool? value) {
-                    try {
-                      if (value == null) {
-                        return;
+            Expanded(
+              flex: 100,
+              child: ListView.builder(
+                itemCount: subtasks?.length ?? 0,
+                itemBuilder: (context, index) {
+                  final subtask = subtasks?[index];
+                  return CheckboxListTile(
+                    enableFeedback: true,
+                    subtitle: subtask?.priority != null
+                        ? Text(
+                            "Priority: ${subtask?.priority}",
+                            style: Theme.of(context)
+                                .textTheme
+                                .bodySmall
+                                ?.copyWith(color: Colors.amber),
+                          )
+                        : null,
+                    value: subtask?.completed,
+                    title: TextField(
+                      readOnly: readOnly,
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: subtask!.title,
+                        hintStyle: Theme.of(context).textTheme.bodyLarge,
+                      ),
+                      style: Theme.of(context).textTheme.bodyLarge,
+                      onSubmitted: (String value) {
+                        final Subtask updatedSubtask =
+                            subtask.copyWith(title: value);
+                        _updateSubtask(updatedSubtask);
+                      },
+                    ),
+                    onChanged: (bool? value) {
+                      try {
+                        if (value == null) {
+                          return;
+                        }
+                        _subtaskComplete(subtask);
+                      } catch (error) {
+                        debugPrint("Error completing subtask: $error");
                       }
-                      _subtaskComplete(subtask);
-                    } catch (error) {
-                      debugPrint("Error completing subtask: $error");
-                    }
-                  },
-                );
-              },
+                    },
+                  );
+                },
+              ),
             ),
             const Spacer(
               flex: 2,
@@ -204,9 +347,11 @@ class _AssignmentScreenState extends ConsumerState<AssignmentScreen> {
                     updateAssignment: _updateAssignment,
                     deleteAssignment: _deleteAssignment,
                     currentAssignment: currentAssignment,
+                    setReadOnly: setReadOnly,
                     size: size,
                     isStar: isStar,
                     isComp: isComp,
+                    readOnly: readOnly,
                     setCurrentAssignmentNull: () {
                       ref
                           .read(currentAssignmentProvider.notifier)
@@ -231,13 +376,16 @@ class ActionButtons extends StatefulWidget {
     required this.size,
     required this.deleteAssignment,
     required this.setCurrentAssignmentNull,
+    required this.setReadOnly,
+    required this.readOnly,
   });
 
   final Assignment? currentAssignment;
   final void Function(Assignment assignment) updateAssignment;
   final void Function(Assignment assignment) deleteAssignment;
+  final void Function(bool readOnly) setReadOnly;
   final Size size;
-  final bool isStar, isComp;
+  final bool isStar, isComp, readOnly;
   final void Function() setCurrentAssignmentNull;
 
   @override
@@ -271,11 +419,11 @@ class ActionButtonsState extends State<ActionButtons> {
           child: const Spacer(),
         ),
         IconButton(
-          icon: const Icon(Icons.edit),
+          icon: Icon(widget.readOnly ? Icons.edit : Icons.edit_off),
           onPressed: () {
-            // Navigate to the edit screen
-            Navigator.of(context).pushNamed(
-                '/edit-assignment?assignmentId=${widget.currentAssignment!.$id}');
+            widget.setReadOnly(
+              !widget.readOnly,
+            );
           },
         ),
         IconButton(
