ac0b9e464334484716b5fb6845e228be48054271
HayHay <dev@hayhay.dev>
2024-05-13 16:20:44 -0400

    Init supabase & clean up consts

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ import 'package:assigngo_rewrite/settings/views/account/account_screen.dart';
 import 'package:assigngo_rewrite/shared/utils/text_theme.dart';
 import 'package:assigngo_rewrite/subjects/views/subjects_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'tabs/tabview.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:assigngo_rewrite/theme.dart';
@@ -20,6 +21,12 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  await Supabase.initialize(
+    url: "https://mzudaknbrzixjkvjqayw.supabase.co",
+    anonKey:
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im16dWRha25icnppeGprdmpxYXl3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTI0MjU3NDgsImV4cCI6MjAyODAwMTc0OH0.b71_fWtic8S4sfNmCMlwLAlzZwhS_lHGBEW1ZQynfsc",
   );
 
   // HomeWidgetInterface? homeWidget;
