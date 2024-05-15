836f4cd26f8d53369caa2d5d9f8d04be32dcf68e
HayHay <dev@hayhay.dev>
2024-05-15 16:52:16 -0400

    Add to macos info plist for deep linking, changes to auth screen using supa auth ui

--- lib/auth/view/auth_screen.dart
+++ lib/auth/view/auth_screen.dart
@@ -1,17 +1,43 @@
 import 'dart:io';
 
+import 'package:assigngo_rewrite/shared/widgets/snackbar.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:assigngo_rewrite/auth/providers/auth_provider.dart';
+import 'package:supabase_auth_ui/supabase_auth_ui.dart';
 
 class AuthScreen extends ConsumerWidget {
   const AuthScreen({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final authState = ref.watch(authProvider);
+    void onSuccess(Session response) {
+      debugPrint("Success: $response");
+
+      if (!kIsWeb && (Platform.isIOS || Platform.isMacOS)) {
+        FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
+          alert: true,
+          badge: true,
+          sound: true,
+        );
+      }
+      debugPrint("Foreground notification options");
+      ref.read(authProvider.notifier).setFirebaseMessaging();
+      debugPrint("Firebase messaging set");
+      Navigator.of(context).popAndPushNamed(
+        '/home',
+      );
+    }
+
+    void onError(error) {
+      debugPrint("Error: $error");
+      SnackbarService.showErrorSnackbar(
+        context,
+        error.toString(),
+      );
+    }
 
     return Scaffold(
         body: Align(
@@ -24,107 +50,49 @@ class AuthScreen extends ConsumerWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            TextFormField(
-              keyboardType: TextInputType.emailAddress,
-              decoration: const InputDecoration(
-                labelText: 'Email',
-                hintText: 'Enter your email',
+            Padding(
+              padding: const EdgeInsets.all(16),
+              child: Text(
+                "Listify",
+                style: Theme.of(context).textTheme.displayLarge,
               ),
-              controller: ref.read(authProvider.notifier).emailController,
-              readOnly: authState.otpSent,
             ),
-            if (authState.otpSent)
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 16),
-                child: TextFormField(
-                  keyboardType: TextInputType.number,
-                  decoration: const InputDecoration(
-                    labelText: 'OTP',
-                    hintText: 'Enter the OTP',
-                  ),
-                  controller: ref.read(authProvider.notifier).otpController,
-                ),
-              ),
-            Column(
+
+            SupaMagicAuth(
+              redirectUrl: kIsWeb ? null : 'listify://signin',
+              onSuccess: onSuccess,
+              onError: onError,
+            ),
+
+            SupaSocialsAuth(
+              socialProviders: const [
+                OAuthProvider.apple,
+                OAuthProvider.google,
+                OAuthProvider.discord
+              ],
+              socialButtonVariant: SocialButtonVariant.icon,
+              colored: true,
+              redirectUrl: kIsWeb ? null : 'listify://signin',
+              onSuccess: onSuccess,
+              onError: onError,
+            ),
+
+            // Sign in with password option
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: FilledButton(
-                    onPressed: () {
-                      ref
-                          .read(authProvider.notifier)
-                          .signIn()
-                          .then((value) async => {
-                                ScaffoldMessenger.of(context).showSnackBar(
-                                  SnackBar(
-                                    content: Text(authState.otpSent
-                                        ? 'OTP verified'
-                                        : 'OTP sent'),
-                                  ),
-                                ),
-                                if (authState.otpSent &&
-                                    authState.error == null)
-                                  {
-                                    debugPrint("OTP verified"),
-                                    if (!kIsWeb &&
-                                        (Platform.isIOS || Platform.isMacOS))
-                                      {
-                                        FirebaseMessaging.instance
-                                            .setForegroundNotificationPresentationOptions(
-                                          alert: true,
-                                          badge: true,
-                                          sound: true,
-                                        ),
-                                      },
-                                    debugPrint(
-                                        "Foreground notification options"),
-                                    ref
-                                        .read(authProvider.notifier)
-                                        .setFirebaseMessaging(),
-                                    debugPrint("Firebase messaging set"),
-                                    Navigator.of(context).popAndPushNamed(
-                                      '/home',
-                                    ),
-                                  }
-                              })
-                          .catchError((error) => {
-                                ScaffoldMessenger.of(context).showSnackBar(
-                                  const SnackBar(
-                                    content:
-                                        Text('Error: Check your OTP code?'),
-                                  ),
-                                ),
-                              });
-                    },
-                    child: const Text('Sign In'),
-                  ),
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pushNamed('/signin/password');
+                  },
+                  child: const Text('Sign In with Password'),
                 ),
-                if (authState.error != null)
-                  Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Text(
-                      'Error: ${authState.error}',
-                      style: const TextStyle(color: Colors.red),
-                    ),
-                  ),
-                // Sign in with password option
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    TextButton(
-                      onPressed: () {
-                        Navigator.of(context).pushNamed('/signin/password');
-                      },
-                      child: const Text('Sign In with Password'),
-                    ),
-                    // ),
-                    TextButton.icon(
-                      icon: const Icon(Icons.launch_rounded),
-                      onPressed: () {},
-                      label: const Text("Privacy Policy"),
-                    ),
-                  ],
-                )
+                // ),
+                TextButton.icon(
+                  icon: const Icon(Icons.launch_rounded),
+                  onPressed: () {},
+                  label: const Text("Privacy Policy"),
+                ),
               ],
             )
           ],
