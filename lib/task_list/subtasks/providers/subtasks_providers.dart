ec21fb4fd1077f993140a84239b9f417110b24a5
HayHay <dev@hayhay.dev>
2024-05-15 14:04:23 -0400

    Update subtasks repository and providers w more methods

--- lib/task_list/subtasks/providers/subtasks_providers.dart
+++ lib/task_list/subtasks/providers/subtasks_providers.dart
@@ -1,3 +1,4 @@
+import 'package:assigngo_rewrite/shared/models/return_model/return_model.dart';
 import 'package:assigngo_rewrite/task_list/subtasks/models/subtasks_model/subtasks_model.dart';
 import 'package:assigngo_rewrite/task_list/subtasks/repositories/subtasks_repository.dart';
 import 'package:flutter/material.dart';
@@ -10,16 +11,51 @@ class SubtasksNotifier extends ChangeNotifier {
 
   List<Subtask> subtasks = [];
 
-  Future<void> toggleSubtaskDone(Subtask subtask) async {
-    final returnModel = await _subtasksRepository.updateSubtask(subtask);
-    if (returnModel.success) {
-      subtasks = subtasks.map((oldSubtask) {
-        if (oldSubtask.$id == subtask.$id) {
-          return subtask;
-        }
-        return oldSubtask;
-      }).toList();
+  Future<ReturnModel> createSubtask(Subtask subtask) async {
+    subtasks.add(subtask);
+    notifyListeners();
+
+    final result = await _subtasksRepository.createSubtask(subtask);
+    if (!result.success) {
+      subtasks.remove(subtask);
       notifyListeners();
     }
+    return result;
+  }
+
+  static _updateSubtaskState(Subtask subtask, List<Subtask> subtasks) {
+    return subtasks.map((oldSubtask) {
+      if (oldSubtask.id == subtask.id) {
+        return subtask;
+      }
+      return oldSubtask;
+    }).toList();
+  }
+
+  Future<ReturnModel> updateSubtask(Subtask subtask) async {
+    subtasks = _updateSubtaskState(subtask, subtasks);
+    notifyListeners();
+
+    final result = await _subtasksRepository.updateSubtask(subtask);
+    if (!result.success) {
+      subtasks = _updateSubtaskState(subtask, subtasks);
+      notifyListeners();
+    }
+    return result;
+  }
+
+  Future<ReturnModel> toggleSubtaskDone(Subtask subtask) async {
+    _updateSubtaskState(
+        subtask.copyWith(completed: !subtask.completed), subtasks);
+    notifyListeners();
+
+    final result = await _subtasksRepository.updateSubtask(subtask);
+    if (!result.success) {
+      _updateSubtaskState(
+          subtask.copyWith(completed: !subtask.completed), subtasks);
+      notifyListeners();
+    }
+
+    return result;
   }
 }
--- lib/task_list/subtasks/repositories/subtasks_repository.dart
+++ lib/task_list/subtasks/repositories/subtasks_repository.dart
@@ -1,21 +1,39 @@
 import 'package:assigngo_rewrite/task_list/subtasks/models/subtasks_model/subtasks_model.dart';
-import 'package:assigngo_rewrite/constants.dart';
 import 'package:assigngo_rewrite/shared/models/return_model/return_model.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 class SubtasksRepository {
   SubtasksRepository();
+  final supabase = Supabase.instance.client;
 
-  Future<ReturnModel> updateSubtask(Subtask subtask) async {
+  Future<ReturnModel> createSubtask(Subtask subtask) async {
     try {
-      final updatedSubtask = await database.updateDocument(
-          databaseId: publicDb,
-          collectionId: "subtasks",
-          documentId: subtask.$id,
-          data: subtask.toJson());
+      final subtaskJson = subtask.toJson();
+      subtaskJson.remove("id");
+
+      final newSubtask =
+          await supabase.from("subtasks").insert(subtaskJson).select();
 
       return ReturnModel(
           success: true,
-          data: updatedSubtask.data,
+          data: Subtask.fromJson(newSubtask.first),
+          message: "Subtask created successfully");
+    } catch (error) {
+      return ReturnModel(success: false, message: error.toString());
+    }
+  }
+
+  Future<ReturnModel> updateSubtask(Subtask subtask) async {
+    try {
+      final updatedSubtask = await supabase
+          .from("subtasks")
+          .update(subtask.toJson())
+          .eq("id", subtask.id!)
+          .select();
+
+      return ReturnModel(
+          success: true,
+          data: Subtask.fromJson(updatedSubtask.first),
           message: "Subtask updated successfully");
     } catch (error) {
       return ReturnModel(success: false, message: error.toString());
