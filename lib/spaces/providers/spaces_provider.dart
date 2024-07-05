cf64d191730bf1b7396e5849703997269f851771
HayHay <dev@hayhay.dev>
2024-07-04 21:32:45 -0400

    feat: Add spaces provider and its generated code

--- lib/spaces/providers/spaces_provider.dart
+++ lib/spaces/providers/spaces_provider.dart
@@ -1,101 +1,113 @@
-import 'package:flutter/foundation.dart';
+import 'package:assigngo_rewrite/categories/repositories/categories_repository.dart';
 import 'package:assigngo_rewrite/spaces/models/spaces_model.dart';
 import 'package:assigngo_rewrite/spaces/repositories/spaces_repository.dart';
-import 'package:assigngo_rewrite/shared/models/return_model/return_model.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:assigngo_rewrite/task_list/repositories/tasks_repository.dart';
+import 'package:flutter/foundation.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
 
-final spacesProvider = ChangeNotifierProvider((ref) => SpacesProvider());
+part 'spaces_provider.g.dart';
 
-class SpacesProvider extends ChangeNotifier {
-  Spaces? _space;
-  Spaces? get space => _space;
+@riverpod
+class SpacesManager extends _$SpacesManager {
+  late final SpacesRepository _repository;
+  late final CategoriesRepository _categoriesRepository;
+  late final TasksRepository _tasksRepository;
 
-  final SpacesRepository _repository = SpacesRepository();
-
-  List<Spaces> _spacesList = [];
-  List<Spaces> get allSpaces => _spacesList;
-
-  bool readOnly = true;
-
-  SpacesProvider() {
-    _fetchAllSpaces();
+  @override
+  FutureOr<List<Spaces>> build() {
+    _repository = SpacesRepository();
+    _categoriesRepository = CategoriesRepository();
+    _tasksRepository = TasksRepository();
+    return fetchSpaces();
   }
 
-  Future<ReturnModel> _fetchAllSpaces() async {
+  Future<List<Spaces>> fetchSpaces() async {
     try {
-      final result = await _repository.getSpaces();
-      _spacesList = result;
-      notifyListeners();
+      final spaces = await _repository.getSpaces();
+      final tasks = await _tasksRepository.getTasks();
+      final categories = await _categoriesRepository.getCategories();
 
-      return ReturnModel(
-          data: _spacesList,
-          message: "Spaces fetched successfully",
-          success: true);
+      List<Spaces> updatedSpaces = [...spaces];
+
+      // Add special spaces
+      updatedSpaces.addAll([
+        const Spaces(title: "Uncategorized", id: -1),
+        const Spaces(title: "Create New Space", id: -2)
+      ]);
+
+      updatedSpaces = updatedSpaces.map((space) {
+        final spaceCategories = categories
+            .where((category) => category.spaceId == null
+                ? space.id == -1
+                : category.spaceId == space.id)
+            .map((category) {
+          final categoryTasks = tasks
+              .where((task) =>
+                  task.categoryId == category.id &&
+                  (task.spaceId == null
+                      ? space.id == -1
+                      : task.spaceId == space.id))
+              .toList();
+          return category.copyWith(tasks: categoryTasks);
+        }).toList();
+
+        final uncategorizedTasks = tasks
+            .where((task) =>
+                task.categoryId == null &&
+                (task.spaceId == null
+                    ? space.id == -1
+                    : task.spaceId == space.id))
+            .toList();
+
+        return space.copyWith(
+          categories: spaceCategories,
+          tasks: uncategorizedTasks,
+        );
+      }).toList();
+
+      debugPrint("Spaces: $updatedSpaces");
+
+      return updatedSpaces;
     } catch (e) {
-      return ReturnModel(
-          message: "Error fetching spaces",
-          success: false,
-          error: e.toString());
+      debugPrint("Error fetching spaces: $e");
+      return [
+        const Spaces(title: "Something went wrong", id: -1),
+      ];
     }
   }
 
-  Future<ReturnModel> fetchSpace(int id) async {
-    try {
-      final result = await _repository.getSpace(id);
-      _space = result;
-      notifyListeners();
-
-      return ReturnModel(
-          data: _space,
-          message: "Space fetched successfully",
-          success: true);
-    } catch (e) {
-      return ReturnModel(
-          message: "Error fetching space",
-          success: false,
-          error: e.toString());
-    }
-  }
-
-  Future<ReturnModel> createSpace(Spaces space) async {
-    try {
-      final result = await _repository.createSpace(space);
-      _spacesList.add(result.data);
-      notifyListeners();
-
-      return ReturnModel(
-          data: result.data,
-          message: "Space created successfully",
-          success: true);
-    } catch (e) {
-      return ReturnModel(
-          message: "Error creating space",
-          success: false,
-          error: e.toString());
-    }
-  }
-
-  Future<ReturnModel> updateSpace(Spaces space) async {
-    try {
-      final result = await _repository.updateSpace(space);
-      _spacesList[_spacesList.indexWhere((element) => element.id == result.data.id)] = result.data;
-      notifyListeners();
-
-      return ReturnModel(
-          data: result.data,
-          message: "Space updated successfully",
-          success: true);
-    } catch (e) {
-      return ReturnModel(
-          message: "Error updating space",
-          success: false,
-          error: e.toString());
-    }
-  }
-
-  onDispose() {
-    // Update all the repositories
+  Future<void> refreshSpaces() async {
+    state = const AsyncValue.loading();
+    state = AsyncValue.data(await fetchSpaces());
   }
 }
+  // Future<ReturnModel> createSpace(Spaces space) async {
+  //   try {
+  //     final result = await _repository.createSpace(space);
+  //     _spacesList.add(result.data);
 
+  //     return ReturnModel(
+  //         data: result.data,
+  //         message: "Space created successfully",
+  //         success: true);
+  //   } catch (e) {
+  //     return ReturnModel(
+  //         message: "Error creating space", success: false, error: e.toString());
+  //   }
+  // }
 
+  // Future<ReturnModel> updateSpace(Spaces space) async {
+  //   try {
+  //     final result = await _repository.updateSpace(space);
+  //     _spacesList[_spacesList
+  //         .indexWhere((element) => element.id == result.data.id)] = result.data;
+
+  //     return ReturnModel(
+  //         data: result.data,
+  //         message: "Space updated successfully",
+  //         success: true);
+  //   } catch (e) {
+  //     return ReturnModel(
+  //         message: "Error updating space", success: false, error: e.toString());
+  //   }
+  // }
---
+++ lib/spaces/providers/spaces_provider.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'spaces_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$spacesManagerHash() => r'8c4f1fdf830079fd4d3d84d76405fd7507010f2d';
+
+/// See also [SpacesManager].
+@ProviderFor(SpacesManager)
+final spacesManagerProvider =
+    AutoDisposeAsyncNotifierProvider<SpacesManager, List<Spaces>>.internal(
+  SpacesManager.new,
+  name: r'spacesManagerProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$spacesManagerHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$SpacesManager = AutoDisposeAsyncNotifier<List<Spaces>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
