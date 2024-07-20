63d1cd45f1b89e184fe802ddb42cbb25af6ffe01
HayHay <dev@hayhay.dev>
2024-07-20 18:19:17 -0400

    Update styling and add edit button

--- lib/spaces/widgets/space_category_tile.dart
+++ lib/spaces/widgets/space_category_tile.dart
@@ -1,5 +1,9 @@
+import 'package:flutter_lucide/flutter_lucide.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:questkeeper/categories/models/categories_model.dart';
-import 'package:questkeeper/shared/utils/hex_color.dart';
+import 'package:questkeeper/categories/views/edit_space_bottom_sheet.dart';
+import 'package:questkeeper/shared/extensions/color_extensions.dart';
+import 'package:questkeeper/shared/extensions/string_extensions.dart';
 import 'package:questkeeper/task_list/models/tasks_model.dart';
 import 'package:questkeeper/task_list/task_item/widgets/task_card.dart';
 import 'package:flutter/material.dart';
@@ -9,10 +13,12 @@ class SpaceCategoryTile extends StatelessWidget {
     super.key,
     required this.category,
     required this.tasks,
+    required this.ref,
   });
 
   final Categories category;
   final List<Tasks>? tasks;
+  final WidgetRef ref;
 
   @override
   Widget build(BuildContext context) {
@@ -20,15 +26,40 @@ class SpaceCategoryTile extends StatelessWidget {
       padding: const EdgeInsets.all(8),
       child: ExpansionTile(
           backgroundColor: category.color != null
-              ? HexColor(category.color!).withOpacity(0.6)
+              ? category.color!
+                  .toColor()
+                  .withOpacity(0.6)
+                  .blendWith(Colors.black)
               : Colors.transparent.withOpacity(0.1),
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(12),
           ),
           enableFeedback: true,
           initiallyExpanded: true,
-          title: Text(category.title,
-              style: Theme.of(context).textTheme.titleMedium),
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                category.title,
+                style: Theme.of(context).textTheme.titleMedium,
+              ),
+              IconButton(
+                icon: const Icon(LucideIcons.pen),
+                onPressed: () {
+                  showCategoryBottomSheet(
+                      context: context, ref: ref, existingCategory: category);
+                },
+              ),
+            ],
+          ),
+          collapsedBackgroundColor: category.color
+                  ?.toColor()
+                  .withOpacity(0.6)
+                  .blendWith(Colors.black) ??
+              Colors.transparent,
+          collapsedShape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
+          ),
           children: tasks?.isNotEmpty == true && tasks != null
               ? tasks!
                   .map((task) => Padding(
