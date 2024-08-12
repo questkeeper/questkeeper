854bea192031ab0d338ca53cd9bf50254249acaa
HayHay <dev@hayhay.dev>
2024-08-12 12:38:15 -0400

    Fomratted date time

--- lib/shared/utils/format_date.dart
+++ lib/shared/utils/format_date.dart
@@ -1,6 +1,15 @@
 import 'package:intl/intl.dart';
 
 String formatDate(DateTime date) {
-  date = date.toLocal();
+  final today = DateTime.now();
+
+  if (date.isAfter(today.subtract(const Duration(days: 1))) &&
+      date.isBefore(today)) {
+    return 'Today at ${DateFormat('hh:mm a').format(date)}';
+  } else if (date.isAfter(today.subtract(const Duration(days: 2))) &&
+      date.isBefore(today.subtract(const Duration(days: 1)))) {
+    return 'Yesterday at ${DateFormat('hh:mm a').format(date)}';
+  }
+
   return DateFormat('MMMM d, y hh:mm a').format(date);
 }
