194ad7559ed150559fe3b04c9bea143abbbebf25
HayHay <dev@hayhay.dev>
2024-08-12 12:36:03 -0400

    Switch to normal notification dot

--- lib/task_list/task_item/widgets/task_notification_dot.dart
+++ lib/task_list/task_item/widgets/task_notification_dot.dart
@@ -2,55 +2,18 @@ import 'package:flutter/material.dart';
 
 enum NotificationDotType { warning, danger }
 
-class NotificationDot extends StatefulWidget {
+class TaskNotificationDot extends StatelessWidget {
   final NotificationDotType notificationType;
-  const NotificationDot({super.key, required this.notificationType});
-
-  @override
-  NotificationDotState createState() => NotificationDotState();
-}
-
-class NotificationDotState extends State<NotificationDot>
-    with SingleTickerProviderStateMixin {
-  late AnimationController _animationController;
-  late Animation<double> _animation;
-
-  @override
-  void initState() {
-    super.initState();
-    _animationController = AnimationController(
-      duration: const Duration(milliseconds: 2500),
-      vsync: this,
-    )..repeat(reverse: true);
-
-    _animation = Tween<double>(begin: 0.9, end: 1.1).animate(
-      CurvedAnimation(parent: _animationController, curve: Curves.easeInOut),
-    );
-  }
-
-  @override
-  void dispose() {
-    _animationController.dispose();
-    super.dispose();
-  }
+  const TaskNotificationDot({super.key, required this.notificationType});
 
   @override
   Widget build(BuildContext context) {
-    return AnimatedBuilder(
-      animation: _animation,
-      builder: (context, child) {
-        return Transform.scale(
-          scale: _animation.value,
-          child: child,
-        );
+    return CircleAvatar(
+      maxRadius: 6.0,
+      backgroundColor: switch (notificationType) {
+        NotificationDotType.warning => Colors.amberAccent,
+        NotificationDotType.danger => Colors.redAccent,
       },
-      child: CircleAvatar(
-        maxRadius: 6.0,
-        backgroundColor: switch (widget.notificationType) {
-          NotificationDotType.warning => Colors.amber,
-          NotificationDotType.danger => Colors.redAccent,
-        },
-      ),
     );
   }
 }
