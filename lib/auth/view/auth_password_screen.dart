5fdbe9314155727d6d09534bbf74d8a51b5d9a62
HayHay <dev@hayhay.dev>
2024-08-21 16:15:24 -0400

    Fix sign in flow for testing

--- lib/auth/view/auth_password_screen.dart
+++ lib/auth/view/auth_password_screen.dart
@@ -1,74 +1,59 @@
-import 'dart:io';
-
 import 'package:questkeeper/auth/providers/auth_provider.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:questkeeper/shared/widgets/snackbar.dart';
+import 'package:supabase_auth_ui/supabase_auth_ui.dart';
 
 class SignInPasswordScreen extends ConsumerWidget {
   const SignInPasswordScreen({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final authState = ref.watch(authProvider);
+    void onSuccess(AuthResponse response) async {
+      if (!response.session!.user.email!.endsWith("@questkeeper.app")) {
+        return SnackbarService.showErrorSnackbar(
+          context,
+          'Not allowed to sign in with email',
+        );
+      }
+
+      await ref.read(authProvider.notifier).setFirebaseMessaging();
+      if (context.mounted) {
+        Navigator.of(context).pushNamedAndRemoveUntil(
+          '/home',
+          (route) => false,
+        );
+      }
+    }
+
+    void onError(error) {
+      debugPrint("Error: $error");
+      SnackbarService.showErrorSnackbar(
+        context,
+        error.toString(),
+      );
+    }
 
     return Scaffold(
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: TextFormField(
-              keyboardType: TextInputType.emailAddress,
-              decoration: const InputDecoration(
-                labelText: 'Email',
-                hintText: 'Enter your email',
-              ),
-              controller: ref.read(authProvider.notifier).emailController,
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: TextFormField(
-              decoration: const InputDecoration(
-                labelText: 'Password',
-                hintText: 'Enter your Password',
-              ),
-              controller: ref.read(authProvider.notifier).otpController,
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: FilledButton(
-              onPressed: () {
-                ref.read(authProvider.notifier).passwordSignIn().then((_) => {
-                      if (authState.error == null)
-                        {
-                          FirebaseMessaging.instance.requestPermission(
-                            provisional: true,
-                          ),
-                          if (Platform.isIOS || Platform.isMacOS)
-                            {
-                              FirebaseMessaging.instance
-                                  .setForegroundNotificationPresentationOptions(
-                                alert: true,
-                                badge: true,
-                                sound: true,
-                              ),
-                            },
-                          ref
-                              .read(authProvider.notifier)
-                              .setFirebaseMessaging(),
-                          Navigator.of(context).popAndPushNamed(
-                            '/home',
-                          ),
-                        }
-                    });
-              },
-              child: const Text("Sign In"),
-            ),
-          ),
+          SupaEmailAuth(
+            onToggleSignIn: (_) {
+              Navigator.pop(context);
+            },
+            onError: onError,
+            onSignInComplete: onSuccess,
+            onSignUpComplete: (AuthResponse response) {
+              Supabase.instance.client.auth.signOut();
+              Navigator.pop(context);
+              SnackbarService.showErrorSnackbar(
+                context,
+                'Not allowed to sign up with email',
+              );
+            },
+          )
         ],
       ),
     );
