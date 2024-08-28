69004ca29490a22902f75affe17138f01c4eacf8
HayHay <dev@hayhay.dev>
2024-08-28 15:00:56 -0400

    Refactor auth provider to include device info, redo firebase messaging to not double fire

--- lib/auth/providers/auth_provider.dart
+++ lib/auth/providers/auth_provider.dart
@@ -1,43 +1,50 @@
-import 'dart:io';
-
+import 'package:device_info_plus/device_info_plus.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:questkeeper/auth/models/auth_state.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
-final authProvider = StateNotifierProvider<AuthNotifier, SignInState>((ref) {
-  return AuthNotifier();
-});
-
-class AuthNotifier extends StateNotifier<SignInState> {
-  AuthNotifier() : super(SignInState(otpSent: false));
+class AuthNotifier {
+  AuthNotifier() : super();
   SupabaseClient supabase = Supabase.instance.client;
+  bool _firebaseMessagingInitialized = false;
 
-  Future<void> saveToken(String token) async {
+  Future<void> _saveToken(String token) async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     String? currentDeviceId = prefs.getString("deviceId");
     String? currentToken = prefs.getString("token");
+    final deviceInfo = await (DeviceInfoPlugin()).deviceInfo;
 
     if (currentToken == token) {
       return;
     }
 
+    if (_firebaseMessagingInitialized == true) {
+      return;
+    }
+
+    final deviceInfoData = Map<String, dynamic>.from({
+      "deviceType": deviceInfo.data["model"],
+      "deviceName": deviceInfo.data["name"],
+      "deviceOs": deviceInfo.data["systemName"],
+      "deviceOsVersion": deviceInfo.data["systemVersion"],
+    });
+
     if (currentDeviceId != null) {
       await supabase.from("profiles").update({
         "previousToken": currentToken,
         "token": token,
-        "deviceType": Platform.operatingSystem,
+        ...deviceInfoData,
       }).eq("deviceId", currentDeviceId);
     } else {
       final deviceId = await supabase.from("profiles").insert({
         "token": token,
+        ...deviceInfoData,
       }).select();
 
       prefs.setString("deviceId", deviceId.first["deviceId"]);
     }
 
-    prefs.setString("token", token);
+    prefs.setString("firebaseToken", token);
   }
 
   // THIS IS ONLy FOR DEBUG PURPOSES
@@ -46,9 +53,7 @@ class AuthNotifier extends StateNotifier<SignInState> {
     String? currentDeviceId = prefs.getString("deviceId");
 
     if (currentDeviceId != null) {
-      await supabase.from("profiles").update({
-        "token": null,
-      }).eq("deviceId", currentDeviceId);
+      await supabase.from("profiles").delete().eq("deviceId", currentDeviceId);
     }
 
     await FirebaseMessaging.instance.deleteToken();
@@ -59,30 +64,29 @@ class AuthNotifier extends StateNotifier<SignInState> {
 
   // Set up FCM
   Future<void> setFirebaseMessaging() async {
-    // await clearToken();
-    String? token;
+    if (_firebaseMessagingInitialized) {
+      return;
+    }
+
     NotificationSettings settings =
         await FirebaseMessaging.instance.requestPermission(
       provisional: true,
     );
 
+    FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
+      alert: true,
+      badge: true,
+      sound: true,
+    );
+
     if (settings.authorizationStatus == AuthorizationStatus.provisional ||
         settings.authorizationStatus == AuthorizationStatus.authorized) {
-      FirebaseMessaging.instance.onTokenRefresh.listen((token) {
-        // Handle new or refreshed APNs token
-        saveToken(token);
-      });
+      await FirebaseMessaging.instance.getAPNSToken();
+      final token = await FirebaseMessaging.instance.getToken();
 
-      FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
-        alert: true,
-        badge: true,
-        sound: true,
-      );
-      // } else {
-      token = await FirebaseMessaging.instance.getToken();
-
-      if (token != null) {
-        saveToken(token);
+      if (token != null && _firebaseMessagingInitialized == false) {
+        await _saveToken(token);
+        _firebaseMessagingInitialized = true;
       } else {
         // Handle the case where the initial token retrieval fails.
         // You might want to retry later or provide user feedback.
