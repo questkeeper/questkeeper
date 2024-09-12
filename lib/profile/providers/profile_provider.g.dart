20b703cc06358a13db3657955d97339971d3409f
HayHay <dev@hayhay.dev>
2024-09-11 22:11:10 -0400

    Create profile state provider for username

---
+++ lib/profile/providers/profile_provider.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'profile_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$profileManagerHash() => r'fdff1e30090b49c9f7b7086026da2336780d1b10';
+
+/// See also [ProfileManager].
+@ProviderFor(ProfileManager)
+final profileManagerProvider =
+    AutoDisposeAsyncNotifierProvider<ProfileManager, Profile>.internal(
+  ProfileManager.new,
+  name: r'profileManagerProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$profileManagerHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$ProfileManager = AutoDisposeAsyncNotifier<Profile>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
