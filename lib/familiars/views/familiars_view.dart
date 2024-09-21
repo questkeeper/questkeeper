a8ee5e5a886b05ae2eeb2d5392282b44c16da73f
HayHay <dev@hayhay.dev>
2024-09-21 13:00:07 -0400

    Inital familiars view

---
+++ lib/familiars/views/familiars_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class FamiliarsView extends StatelessWidget {
+  const FamiliarsView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('Familiars View'),
+    );
+  }
+}
