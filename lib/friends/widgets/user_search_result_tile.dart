c9d6864a3b9a66649ce4539208999fae29178993
HayHay <dev@hayhay.dev>
2024-09-28 14:01:29 -0400

    Refactor to use consumer widget

--- lib/friends/widgets/user_search_result_tile.dart
+++ lib/friends/widgets/user_search_result_tile.dart
@@ -1,21 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_lucide/flutter_lucide.dart';
 import 'package:questkeeper/friends/models/user_search_model.dart';
-import 'package:questkeeper/friends/repositories/friend_repository.dart';
+import 'package:questkeeper/friends/providers/friends_provider.dart';
+import 'package:questkeeper/friends/providers/friends_request_provider.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class UserSearchResultTile extends StatelessWidget {
+class UserSearchResultTile extends ConsumerWidget {
   const UserSearchResultTile({
     super.key,
     required this.user,
-    required this.context,
   });
 
   final UserSearchResult user;
-  final BuildContext context;
 
   @override
-  Widget build(BuildContext context) {
-    final FriendRepository repository = FriendRepository();
+  Widget build(BuildContext context, WidgetRef ref) {
+    // Get the ref to the request state
+    final friendRequestManager =
+        ref.watch(friendsRequestManagerProvider.notifier);
+    final friendsManager = ref.watch(friendsManagerProvider.notifier);
     final isPending = user.status == 'pending';
     final isFriend = user.status == 'friend';
     return Card(
@@ -23,27 +26,42 @@ class UserSearchResultTile extends StatelessWidget {
       elevation: 3,
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
       color: Theme.of(context).focusColor,
-      child: ListTile(
-        title: Text(user.username),
-        subtitle: Text(!isFriend && !isPending ? 'Not friends' : user.status!),
-        trailing: isFriend
-            ? IconButton(
-                icon: const Icon(LucideIcons.check),
-                onPressed: () {},
-              )
-            : isPending
-                ? IconButton(
-                    icon: const Icon(LucideIcons.x),
-                    onPressed: () {
-                      repository.removeFriend(user.username);
-                    },
-                  )
-                : IconButton(
-                    icon: const Icon(LucideIcons.plus),
-                    onPressed: () {
-                      repository.addFriend(user.username);
-                    },
-                  ),
+      key: key,
+      child: Column(
+        children: [
+          ListTile(
+            title: Text(user.username),
+            subtitle:
+                Text(!isFriend && !isPending ? 'Not friends' : user.status!),
+            trailing: ElevatedButton.icon(
+              onPressed: () {
+                if (isFriend) {
+                  friendsManager.removeFriend(user.username);
+                } else if (isPending) {
+                  friendRequestManager.rejectRequest(user);
+                } else {
+                  friendRequestManager.acceptRequest(user);
+                }
+              },
+              icon: Icon(
+                isFriend
+                    ? LucideIcons.x
+                    : isPending
+                        ? LucideIcons.x
+                        : LucideIcons.plus,
+                size: 16,
+              ),
+              label: Text(
+                isFriend
+                    ? 'Remove'
+                    : isPending
+                        ? 'Reject'
+                        : 'Accept',
+                style: const TextStyle(fontSize: 12),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
