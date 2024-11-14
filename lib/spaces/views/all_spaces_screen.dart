f4a1080f521a17edc3048dbb205cbf45f8ef14de
HayHay <dev@hayhay.dev>
2024-11-14 09:15:38 -0500

    Hide/show game container based on page and abstract circle progress bar

--- lib/spaces/views/all_spaces_screen.dart
+++ lib/spaces/views/all_spaces_screen.dart
@@ -1,14 +1,15 @@
 import 'package:questkeeper/familiars/widgets/familiars_widget_game.dart';
-import 'package:questkeeper/spaces/models/spaces_model.dart';
 import 'package:questkeeper/spaces/providers/game_height_provider.dart';
 import 'package:questkeeper/spaces/providers/page_provider.dart';
 import 'package:questkeeper/spaces/providers/spaces_provider.dart';
+import 'package:questkeeper/spaces/views/edit_space_bottom_sheet.dart';
 import 'package:questkeeper/spaces/widgets/animated_game_container.dart';
-import 'package:questkeeper/spaces/widgets/circle_bar.dart';
+import 'package:questkeeper/spaces/widgets/circle_progress_bar.dart';
 import 'package:questkeeper/spaces/widgets/space_card.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_lucide/flutter_lucide.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
 
 class AllSpacesScreen extends ConsumerStatefulWidget {
   const AllSpacesScreen({super.key});
@@ -20,44 +21,81 @@ class AllSpacesScreen extends ConsumerStatefulWidget {
 class _AllSpacesState extends ConsumerState<AllSpacesScreen> {
   late PageController _pageController;
   final FamiliarsWidgetGame _game = FamiliarsWidgetGame();
-  int currentPageValue = 0;
+  ValueNotifier<int> currentPageValue = ValueNotifier(0);
+  ValueNotifier<bool> showGameNotifier = ValueNotifier(true);
 
   @override
   void initState() {
     super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _pageController = ref.read(pageControllerProvider);
+      _pageController.addListener(_updatePage);
+    });
+  }
+
+  void _updatePage() {
+    if (mounted) {
+      currentPageValue.value = _pageController.page?.round() ?? 0;
+    }
+  }
+
+  @override
+  void dispose() {
+    currentPageValue.dispose();
+    showGameNotifier.dispose();
+    _pageController.removeListener(_updatePage);
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     final spacesAsync = ref.watch(spacesManagerProvider);
     final heightFactor = ref.watch(gameHeightProvider);
-    _pageController = ref.watch(pageControllerProvider);
 
     return SafeArea(
       child: spacesAsync.when(
         loading: () => const Center(child: CircularProgressIndicator()),
-        error: (error, stack) => Center(child: Text('Error: $error')),
+        error: (error, stack) {
+          Sentry.captureException(
+            error,
+            stackTrace: stack,
+          );
+          return Center(child: Text('Error: $error'));
+        },
         data: (spaces) {
           return Stack(
             children: [
               Column(
                 children: [
                   Container(
-                    margin: const EdgeInsets.all(4.0),
+                    margin: EdgeInsets.symmetric(horizontal: 4),
                     child: AnimatedGameContainer(
-                        game: _game, heightFactor: heightFactor),
+                      game: _game,
+                      heightFactor: heightFactor,
+                    ),
                   ),
                   Expanded(
                     child: PageView.builder(
                       controller: _pageController,
                       onPageChanged: (page) {
-                        // Handle page changes - animate familiar if needed
-                        currentPageValue = page;
+                        currentPageValue = ValueNotifier(page);
                         final isForward =
                             page > (ref.read(pageControllerProvider).page ?? 0);
                         _game.animateEntry(
                           isForward ? Direction.left : Direction.right,
                         );
+
+                        if (currentPageValue.value == spaces.length) {
+                          showSpaceBottomSheet(
+                            context: context,
+                            ref: ref,
+                          );
+                          ref.read(gameHeightProvider.notifier).state = 0.3;
+                        }
+
+                        if (!isForward && page == spaces.length - 1) {
+                          ref.read(gameHeightProvider.notifier).state = 1.0;
+                        }
                       },
                       itemCount: spaces.length + 1,
                       itemBuilder: (context, index) {
@@ -78,56 +116,11 @@ class _AllSpacesState extends ConsumerState<AllSpacesScreen> {
                   ),
                 ],
               ),
-              _buildBottomBar(spaces),
+              CircleProgressBar(spaces: spaces)
             ],
           );
         },
       ),
     );
-  }
-
-  Align _buildBottomBar(List<Spaces> spaces) {
-    return Align(
-        alignment: Alignment.bottomCenter,
-        child: Container(
-          margin: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              Row(
-                mainAxisSize: MainAxisSize.min,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  for (int i = 0; i < spaces.length; i++)
-                    if (i == currentPageValue) ...[
-                      const CircleBar(isActive: true),
-                      // circleBar(true)
-                    ] else
-                      GestureDetector(
-                        onTap: () {
-                          _pageController.animateToPage(i,
-                              duration: const Duration(milliseconds: 300),
-                              curve: Curves.easeInOut);
-                        },
-                        child: const CircleBar(isActive: false),
-                      ),
-                  IconButton(
-                      onPressed: () {
-                        setState(() {
-                          currentPageValue = spaces.length;
-                          _pageController.animateToPage(spaces.length,
-                              duration: const Duration(milliseconds: 300),
-                              curve: Curves.easeInOut);
-                        });
-                      },
-                      icon: const Icon(LucideIcons.plus,
-                          size: 24, color: Colors.blue)),
-                ],
-              ),
-              const SizedBox(height: 8.0),
-            ],
-          ),
-        ));
   }
 }
