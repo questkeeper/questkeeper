bf4ed039cde3f7ef1098057345061747a7f8ea3c
flerouwu <flero@flero.dev>
2024-11-22 15:49:33 +1030

    Add faker spaces and friends in debug mode

--- lib/friends/providers/friends_provider.dart
+++ lib/friends/providers/friends_provider.dart
@@ -1,3 +1,8 @@
+import 'dart:math';
+
+import 'package:uuid/uuid.dart';
+import 'package:faker/faker.dart';
+import 'package:questkeeper/constants.dart';
 import 'package:questkeeper/friends/models/friend_model.dart';
 import 'package:questkeeper/friends/repositories/friend_repository.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
@@ -18,6 +23,20 @@ class FriendsManager extends _$FriendsManager {
   Future<List<Friend>> fetchFriends() async {
     try {
       var friends = await _repository.fetchFriends();
+
+      if (isDebug) {
+        var uuid = Uuid();
+        var random = Random();
+
+        for (var i = 0; i < 20; i++) {
+          friends.add(Friend(
+              userId: uuid.v4(),
+              username: faker.internet.userName(),
+              points: random.nextInt(100000).toString(),
+          ));
+        }
+      }
+
       return friends;
     } catch (e) {
       state = AsyncValue.error(e, StackTrace.current);
