190699fc8c3d7ae1f4996032314f1334188098c5
HayHay <dev@hayhay.dev>
2024-11-22 01:10:23 -0500

    New settings screen

--- lib/settings/views/settings_screen.dart
+++ lib/settings/views/settings_screen.dart
@@ -1,50 +1,100 @@
 import 'package:feedback_sentry/feedback_sentry.dart';
 import 'package:flutter_lucide/flutter_lucide.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:questkeeper/profile/model/profile_model.dart';
+import 'package:questkeeper/profile/providers/profile_provider.dart';
 import 'package:questkeeper/settings/widgets/settings_card.dart';
 import 'package:flutter/material.dart';
+import 'package:questkeeper/shared/widgets/avatar_widget.dart';
 import 'package:questkeeper/shared/widgets/snackbar.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
-class SettingsScreen extends StatelessWidget {
+class SettingsScreen extends ConsumerWidget {
   const SettingsScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     void notYetImplemented() {
       SnackbarService.showErrorSnackbar(context, 'Not yet implemented');
     }
 
     final user = Supabase.instance.client.auth.currentUser;
 
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          user?.email?.split('@')[0] ?? 'Settings',
-          maxLines: 1,
-          overflow: TextOverflow.ellipsis,
-          style: Theme.of(context).textTheme.headlineLarge,
-        ),
-      ),
-      body: SingleChildScrollView(
-        child: Container(
-          padding: const EdgeInsets.all(20),
-          child: Column(
-            children: [
-              Column(
-                children: [
-                  const Divider(),
-                  SettingsCard(
-                      title: 'Notifications',
-                      description: 'Manage your notifications',
-                      icon: LucideIcons.bell_ring,
-                      onTap: notYetImplemented),
-                  SettingsCard(
-                      title: 'Theme',
-                      description: 'Change the app theme',
-                      icon: LucideIcons.palette,
-                      onTap: notYetImplemented),
-                  const Divider(),
-                  SettingsCard(
+    return SafeArea(
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Container(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              children: [
+                FutureBuilder(
+                  future: ref.watch(profileManagerProvider.future),
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      final userProfile = snapshot.data as Profile;
+                      return Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          AvatarWidget(
+                            seed: userProfile.user_id,
+                          ),
+                          const SizedBox(width: 20),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    userProfile.username,
+                                    style: Theme.of(context)
+                                        .textTheme
+                                        .headlineSmall,
+                                  ),
+                                  userProfile.isPro == true
+                                      ? Text('PRO',
+                                          style: TextStyle(
+                                              color: Colors.greenAccent))
+                                      : const SizedBox(),
+                                ],
+                              ),
+                              const SizedBox(height: 5),
+                              Text(
+                                '${userProfile.points} points',
+                                style: Theme.of(context).textTheme.bodyLarge,
+                              ),
+                              Text(
+                                'Account since ${userProfile.created_at.split("T")[0]}',
+                                style: Theme.of(context).textTheme.bodyMedium,
+                              ),
+                            ],
+                          ),
+                        ],
+                      );
+                    } else {
+                      return const LinearProgressIndicator();
+                    }
+                  },
+                ),
+                Column(
+                  children: [
+                    const SizedBox(height: 10),
+                    const Divider(),
+                    SettingsCard(
+                        title: 'Notifications',
+                        description: 'Manage your notifications',
+                        icon: LucideIcons.bell_ring,
+                        onTap: notYetImplemented),
+                    SettingsCard(
+                        title: 'Theme',
+                        description: 'Change the app theme',
+                        icon: LucideIcons.palette,
+                        onTap: notYetImplemented),
+                    const Divider(),
+                    SettingsCard(
                       title: 'Feedback',
                       description: 'Send us your feedback',
                       icon: LucideIcons.bug,
@@ -55,35 +105,39 @@ class SettingsScreen extends StatelessWidget {
                         BetterFeedback.of(context).showAndUploadToSentry(
                             name: user?.id ?? 'Unknown',
                             email: user?.email ?? 'Unknown@questkeeper.app');
-                      }),
-                  const Divider(),
-                  SettingsCard(
-                      title: 'About',
-                      description: 'About the app',
-                      icon: LucideIcons.info,
-                      onTap: () =>
-                          Navigator.pushNamed(context, '/settings/about')),
-                  SettingsCard(
+                      },
+                    ),
+                    SettingsCard(
+                        title: 'About',
+                        description: 'About the app',
+                        icon: LucideIcons.info,
+                        onTap: () =>
+                            Navigator.pushNamed(context, '/settings/about')),
+                    SettingsCard(
                       title: 'Sign out',
-                      description: 'Sign out',
+                      description: 'Sign out and remove local data',
                       icon: LucideIcons.log_out,
-                      backgroundColor: Colors.red,
+                      iconColor: Colors.redAccent,
                       onTap: () async {
-                        await Supabase.instance.client.auth
-                            .signOut()
-                            .then((value) => {
-                                  if (context.mounted)
-                                    Navigator.pushReplacementNamed(
-                                        context, "/signin"),
-                                });
-                      }),
-                  const Divider(),
-                  const Text(
-                      textAlign: TextAlign.center,
-                      'To delete your account, please message us at contact@questkeeper.app'),
-                ],
-              ),
-            ],
+                        await Supabase.instance.client.auth.signOut().then(
+                              (value) => {
+                                if (context.mounted)
+                                  Navigator.pushReplacementNamed(
+                                      context, "/signin"),
+                              },
+                            );
+                      },
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 8.0),
+                      child: const Text(
+                          textAlign: TextAlign.center,
+                          'To delete your account, please message us at contact@questkeeper.app'),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
