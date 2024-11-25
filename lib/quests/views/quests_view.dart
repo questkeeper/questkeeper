424b50eb3fa1fafc21a4613968d17702da6f7614
HayHay <dev@hayhay.dev>
2024-11-25 18:21:55 -0500

    Revamp quests view a little?

--- lib/quests/views/quests_view.dart
+++ lib/quests/views/quests_view.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:questkeeper/quests/models/badge.dart' as badge_model;
+import 'package:questkeeper/quests/providers/badge_providers.dart';
 import 'package:questkeeper/shared/extensions/color_extensions.dart';
 
 class QuestsView extends ConsumerWidget {
@@ -8,21 +11,56 @@ class QuestsView extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
-      body: Center(
-        child: Container(
-          padding: EdgeInsets.all(16),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(16),
-            gradient: LinearGradient(
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-              colors: Colors.purple.toCardGradientColor(),
+      body: Container(
+        padding: EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: Colors.purple.toCardGradientColor(),
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Badges",
+              style: Theme.of(context).textTheme.headlineMedium,
             ),
-          ),
-          child: Text(
-            "Coming Soon...",
-            style: Theme.of(context).textTheme.headlineSmall,
-          ),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: ref.watch(badgeManagerProvider).when(
+                    data: (badges) {
+                      return Row(
+                        children: badges
+                            .map(
+                              (badge) => badgeCard(context, badge),
+                            )
+                            .toList(),
+                      );
+                    },
+                    loading: () => CircularProgressIndicator(),
+                    error: (error, stackTrace) => Text("Error: $error"),
+                  ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget badgeCard(BuildContext context, badge_model.Badge badge) {
+    return Card(
+      child: Padding(
+        padding: EdgeInsets.all(8),
+        child: Column(
+          children: [
+            Text(badge.name, style: Theme.of(context).textTheme.headlineSmall),
+            Text(badge.description),
+            Text(badge.tier.toString()),
+            Text(badge.resetMonthly ? "Monthly" : "One time"),
+          ],
         ),
       ),
     );
