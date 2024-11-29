cfb940d46f56ed684c010795c2627111a304991a
HayHay <dev@hayhay.dev>
2024-11-29 11:44:02 -0500

    Impl friendly display and dismiss notification + better ordered notifications

--- lib/settings/views/notifications/notifications_screen.dart
+++ lib/settings/views/notifications/notifications_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_lucide/flutter_lucide.dart';
+import 'package:questkeeper/shared/utils/format_date.dart';
+import 'package:questkeeper/shared/widgets/snackbar.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 class NotificationsScreen extends StatefulWidget {
@@ -15,6 +18,45 @@ class _NotificationsScreenState extends State<NotificationsScreen> {
   @override
   void initState() {
     super.initState();
+  }
+
+  Future<void> dismissNotification(int id) async {
+    try {
+      await supabaseClient
+          .from("notifications")
+          .update({"read": true}).eq("id", id);
+      mounted
+          ? SnackbarService.showSuccessSnackbar(
+              context,
+              "Notification dismissed",
+            )
+          : null;
+    } catch (e) {
+      mounted
+          ? SnackbarService.showErrorSnackbar(
+              context,
+              "Failed to dismiss notification",
+            )
+          : null;
+    }
+  }
+
+  Future<List<Map<String, dynamic>>> supabaseQueryBuilder() async {
+    switch (selected) {
+      case _NotificationType.previous:
+        return await supabaseClient
+            .from("notifications")
+            .select()
+            .eq("read", false)
+            .order("created_at", ascending: false)
+            .limit(25);
+      case _NotificationType.upcoming:
+        return await supabaseClient
+            .from("notification_schedule")
+            .select()
+            .order("scheduled_at", ascending: true)
+            .limit(25);
+    }
   }
 
   @override
@@ -37,13 +79,14 @@ class _NotificationsScreenState extends State<NotificationsScreen> {
                 selected: {selected.name},
                 onSelectionChanged: (selections) {
                   setState(() {
-                    selected = _NotificationType.values.firstWhere((it) => it.name == selections.first);
+                    selected = _NotificationType.values
+                        .firstWhere((it) => it.name == selections.first);
                   });
                 },
               ),
               Expanded(
                 child: FutureBuilder(
-                  future: supabaseClient.from(selected.table).select(),
+                  future: supabaseQueryBuilder(),
                   builder: (context, snapshot) {
                     if (snapshot.connectionState == ConnectionState.waiting) {
                       return const Center(
@@ -64,8 +107,12 @@ class _NotificationsScreenState extends State<NotificationsScreen> {
 
                           switch (selected) {
                             case _NotificationType.previous:
-                              return PreviousNotification(data: notification);
-
+                              return PreviousNotification(
+                                  data: notification,
+                                  onDismiss: () async {
+                                    await dismissNotification(
+                                        notification["id"] as int);
+                                  });
                             case _NotificationType.upcoming:
                               return UpcomingNotification(data: notification);
                           }
@@ -113,13 +160,44 @@ class PreviousNotification extends Notification {
   const PreviousNotification({
     super.key,
     required super.data,
+    required this.onDismiss,
   });
+
+  final Future<void> Function() onDismiss;
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      title: Text(data["message"]),
-      subtitle: Text(data["type"]),
+    return Dismissible(
+      key: ValueKey(data["id"]),
+      confirmDismiss: (direction) async {
+        if (direction == DismissDirection.endToStart) {
+          await onDismiss();
+          return true;
+        }
+
+        return false;
+      },
+      direction: DismissDirection.endToStart,
+      background: Container(
+        color: Colors.redAccent,
+        alignment: Alignment.centerRight,
+        padding: const EdgeInsets.only(right: 20.0),
+        child: const Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Text(
+              "Mark as read",
+              style: TextStyle(color: Colors.black, fontSize: 16.0),
+            ),
+            SizedBox(width: 8.0),
+            Icon(LucideIcons.check, color: Colors.black, size: 32.0),
+          ],
+        ),
+      ),
+      child: ListTile(
+        title: Text(data["message"]),
+        subtitle: Text(notificationTypeDisplay[data["type"]] ?? "Unknown"),
+      ),
     );
   }
 }
@@ -133,19 +211,23 @@ class UpcomingNotification extends Notification {
   @override
   Widget build(BuildContext context) {
     var title = data["title"] as String?;
-    var subtitle = data["message"] as String?;
+    final scheduledAt = data["scheduled_at"] as String;
+    final scheduledAtDateTime = formatDate(DateTime.parse(scheduledAt));
 
     if (title == null || title.isEmpty) {
       title = "Missing title";
     }
 
-    if (subtitle == null || subtitle.isEmpty) {
-      subtitle = "Missing subtitle";
-    }
-
     return ListTile(
       title: Text(title),
-      subtitle: Text(subtitle),
+      subtitle: Text("Scheduled for $scheduledAtDateTime"),
     );
   }
 }
+
+const notificationTypeDisplay = {
+  "friendRequest": "Friend Request",
+  "nudge": "Nudge",
+  "badgeEarned": "Badge Earned",
+  "pointsEarned": "Points Earned",
+};
