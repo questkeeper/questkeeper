1b158d1879b083ebb902a46382e5b1fcdccd7a63
HayHay <dev@hayhay.dev>
2024-12-02 14:47:00 -0500

    Redesign for bg

--- lib/auth/widgets/auth_space_card.dart
+++ lib/auth/widgets/auth_space_card.dart
@@ -1,29 +1,66 @@
-import 'package:questkeeper/shared/extensions/color_extensions.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:questkeeper/shared/widgets/pulsating_painter.dart';
 
-class AuthSpaceCard extends ConsumerWidget {
+class AuthSpaceCard extends ConsumerStatefulWidget {
   final Widget currentSpaceScreen;
   final Color? baseColor;
 
-  const AuthSpaceCard(
-      {super.key, required this.currentSpaceScreen, this.baseColor});
+  const AuthSpaceCard({
+    super.key,
+    required this.currentSpaceScreen,
+    this.baseColor,
+  });
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final localBaseColor = baseColor ?? const Color.fromARGB(255, 126, 84, 223);
+  AuthSpaceCardState createState() => AuthSpaceCardState();
+}
+
+class AuthSpaceCardState extends ConsumerState<AuthSpaceCard>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(seconds: 60),
+      vsync: this,
+    )..repeat(reverse: true);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final localBaseColor =
+        widget.baseColor ?? const Color.fromARGB(255, 126, 84, 223);
+
     return Card(
       margin: const EdgeInsets.all(12),
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(12),
-          gradient: LinearGradient(
-            colors: localBaseColor.toCardGradientColor(),
-            begin: Alignment.topLeft,
-            end: Alignment.bottomCenter,
-          ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(12),
+        child: Stack(
+          children: [
+            AnimatedBuilder(
+              animation: _controller,
+              builder: (context, child) {
+                return CustomPaint(
+                  painter: PulsatingPainter(
+                    color: localBaseColor,
+                    progress: _controller.value,
+                  ),
+                  child: Container(),
+                );
+              },
+            ),
+            widget.currentSpaceScreen,
+          ],
         ),
-        child: currentSpaceScreen,
       ),
     );
   }
