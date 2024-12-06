c6f9df48219d9528b808c26b9379734e31252b2c
HayHay <dev@hayhay.dev>
2024-12-06 16:30:54 -0500

    Refactor profile management for new http service

--- lib/profile/repositories/profile_repository.dart
+++ lib/profile/repositories/profile_repository.dart
@@ -1,70 +1,95 @@
-import 'dart:convert';
-
-import 'package:http/http.dart' as http;
-import 'package:questkeeper/constants.dart';
 import 'package:questkeeper/shared/models/return_model/return_model.dart';
+import 'package:questkeeper/shared/utils/http_service.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 class ProfileRepository {
   final Session session = Supabase.instance.client.auth.currentSession!;
-  final header = {
-    'Content-Type': 'application/json',
-    'Accept': 'application/json',
-    'Authorization':
-        'Bearer ${Supabase.instance.client.auth.currentSession!.accessToken}'
-  };
-  Future<ReturnModel> getMyProfile() async {
-    final response = await http.get(Uri.parse('$baseApiUri/social/profile/me'),
-        headers: header);
+  final HttpService _httpService = HttpService();
 
-    if (response.statusCode != 200 || response.body.isEmpty) {
-      return ReturnModel(
-          message: "Error fetching profile",
-          success: false,
-          error: response.body);
-    } else {
-      return ReturnModel(
+  Future<ReturnModel> getMyProfile() async {
+    try {
+      final response = await _httpService.get('/social/profile/me');
+
+      if (response.statusCode == 200) {
+        return ReturnModel(
           message: "Profile fetched successfully",
           success: true,
-          data: response.body);
+          data: response.data,
+        );
+      } else {
+        return ReturnModel(
+          message: "Error fetching profile",
+          success: false,
+          error: response.data.toString(),
+        );
+      }
+    } catch (error) {
+      Sentry.captureException(error);
+      return ReturnModel(
+        message: "Error fetching profile",
+        success: false,
+        error: error.toString(),
+      );
     }
   }
 
   Future<ReturnModel> updateUsername(String username) async {
-    final response = await http.post(Uri.parse('$baseApiUri/social/profile/me'),
-        body: const JsonEncoder().convert({"username": username}),
-        headers: header);
+    try {
+      final response = await _httpService.post('/social/profile/me', data: {
+        "username": username,
+      });
 
-    if (response.statusCode != 200) {
-      return ReturnModel(
+      if (response.statusCode == 200) {
+        return const ReturnModel(
+          message: "Username updated successfully",
+          success: true,
+        );
+      } else {
+        return ReturnModel(
           message: response.statusCode == 409
               ? "Username already taken"
               : "Error updating username",
           success: false,
           error: response.statusCode == 409
               ? "Username already taken"
-              : response.body);
-    } else {
-      return const ReturnModel(
-          message: "Username updated successfully", success: true);
+              : response.data,
+        );
+      }
+    } catch (error) {
+      Sentry.captureException(error);
+      return ReturnModel(
+        message: "Error updating username",
+        success: false,
+        error: error.toString(),
+      );
     }
   }
 
   Future<ReturnModel> getProfile(String username) async {
-    final response = await http.get(
-        Uri.parse('$baseApiUri/social/profile/$username'),
-        headers: header);
+    try {
+      final response = await _httpService.get('/social/profile/$username');
 
-    if (response.statusCode != 200) {
-      return ReturnModel(
-          message: "Error fetching profile",
-          success: false,
-          error: response.body);
-    } else {
-      return ReturnModel(
+      if (response.statusCode == 200) {
+        return ReturnModel(
           message: "Profile fetched successfully",
           success: true,
-          data: response.body);
+          data: response.data.toString(),
+        );
+      } else {
+        return ReturnModel(
+          message: "Error fetching profile",
+          success: false,
+          error: response.data.toString(),
+        );
+      }
+    } catch (error) {
+      Sentry.captureException(error);
+      return ReturnModel(
+        message: "Error fetching profile",
+        success: false,
+        error: error.toString(),
+      );
     }
   }
 }
