69aa9fd4a5988f524a7daa4dd2735a747aa9d04a
HayHay <dev@hayhay.dev>
2024-12-15 00:55:23 -0500

    Slight fix for trophy stuff

--- lib/shared/widgets/trophy_avatar.dart
+++ lib/shared/widgets/trophy_avatar.dart
@@ -4,19 +4,19 @@ enum TrophyType { gold, silver, bronze }
 
 class TrophyAvatar extends StatelessWidget {
   const TrophyAvatar(
-      {super.key, this.trophyType = TrophyType.bronze, this.radius = 50});
+      {super.key, this.trophyType = TrophyType.bronze, this.scale = 1});
 
   final TrophyType trophyType;
-  final double radius;
+  final double scale;
   @override
   Widget build(BuildContext context) {
     return CircleAvatar(
-      radius: radius,
       backgroundColor: Colors.transparent,
       child: Image.asset(
         'assets/trophy/trophy_${trophyType.toString().split('.').last}.png',
         width: 32,
         height: 32,
+        scale: scale,
       ),
     );
   }
