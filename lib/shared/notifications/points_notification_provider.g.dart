e664d9f50cdaa76f2f53064bfe66ebc57bebea13
HayHay <dev@hayhay.dev>
2024-12-29 19:59:58 -0500

    Points state handler

---
+++ lib/shared/notifications/points_notification_provider.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'points_notification_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$pointsNotificationManagerHash() =>
+    r'f2ea7de2eacb3ab3a885c20aff3be95355eb649d';
+
+/// See also [PointsNotificationManager].
+@ProviderFor(PointsNotificationManager)
+final pointsNotificationManagerProvider = AutoDisposeNotifierProvider<
+    PointsNotificationManager, Map<String, dynamic>>.internal(
+  PointsNotificationManager.new,
+  name: r'pointsNotificationManagerProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$pointsNotificationManagerHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$PointsNotificationManager = AutoDisposeNotifier<Map<String, dynamic>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
