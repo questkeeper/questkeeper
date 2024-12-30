db3386e17e62ae12dc11ffd53e2f8c9e7fbbecb0
HayHay <dev@hayhay.dev>
2024-12-29 21:42:42 -0500

    abstract logic a bit

--- lib/shared/notifications/notification_handler.dart
+++ lib/shared/notifications/notification_handler.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -11,45 +13,52 @@ class NotificationHandler {
 
   static Future<void> initialize(WidgetRef ref) async {
     // Handle regular foreground messages
-    FirebaseMessaging.onMessage.listen((RemoteMessage message) async {
-      if (message.data.isNotEmpty) {
-        final data = Map<String, dynamic>.from(message.data);
-        debugPrint(data.toString());
-        if (data.containsKey('message')) {
-          if (data.containsKey("messageType")) {
-            switch (data['messageType']) {
-              case "quest":
-                NotificationService().showMessage(data['message']);
-                break;
-              case "points_rewarded":
-                debugPrint("points_rewarded");
-                ref
-                    .read(pointsNotificationManagerProvider.notifier)
-                    .showBadgeTemporarily(
-                      int.tryParse(data['pointsValue']) ?? 0,
-                    );
 
-                // Update the profile cache on device
-                await ref.read(profileManagerProvider.notifier).fetchProfile();
-                break;
-              default:
-                NotificationService().showMessage(data['message']);
-            }
-          } else {
-            NotificationService().showMessage(data['message']);
+    if (!Platform.isIOS || !Platform.isMacOS) {
+      FirebaseMessaging.onMessage.listen((RemoteMessage message) async {
+        if (message.data.isNotEmpty) {
+          final data = Map<String, dynamic>.from(message.data);
+          if (data.containsKey('message')) {
+            _messageTypeHandlers(ref, data);
           }
         }
-      }
-    });
-
-    // Set up platform channel listener for iOS data messages
-    platform.setMethodCallHandler((call) async {
-      if (call.method == 'onDataMessage') {
-        final data = Map<String, dynamic>.from(call.arguments);
-        if (data.containsKey('message')) {
-          NotificationService().showMessage(data['message']);
+      });
+    } else {
+      // Set up platform channel listener for iOS data messages
+      platform.setMethodCallHandler((call) async {
+        if (call.method == 'onDataMessage') {
+          final data = Map<String, dynamic>.from(call.arguments);
+          if (data.containsKey('message')) {
+            _messageTypeHandlers(ref, data);
+          }
         }
+      });
+    }
+  }
+
+  static void _messageTypeHandlers(
+      WidgetRef ref, Map<String, dynamic> data) async {
+    if (data.containsKey("messageType")) {
+      switch (data['messageType']) {
+        case "quest":
+          NotificationService().showMessage(data['message']);
+          break;
+        case "points_rewarded":
+          debugPrint("points_rewarded");
+          // Update the profile cache on device
+          await ref.read(profileManagerProvider.notifier).fetchProfile();
+
+          ref
+              .read(pointsNotificationManagerProvider.notifier)
+              .showBadgeTemporarily(
+                int.tryParse(data['pointsValue']) ?? 0,
+              );
+          break;
+        default:
+          NotificationService().showMessage(data['message']);
       }
-    });
+    } else {
+      NotificationService().showMessage(data['message']);
+    }
   }
 }
