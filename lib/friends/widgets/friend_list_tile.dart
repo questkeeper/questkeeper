10d157c78f701f187ffc2bbf2a5667332164ea26
HayHay <dev@hayhay.dev>
2025-01-08 17:13:50 -0500

    Add user to friends list

--- lib/friends/widgets/friend_list_tile.dart
+++ lib/friends/widgets/friend_list_tile.dart
@@ -24,89 +24,102 @@ class FriendListTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
-      tileColor: position.isEven
-          ? Theme.of(context).colorScheme.surfaceContainerLow
-          : Theme.of(context).colorScheme.surfaceContainerHigh,
-      leading: AvatarWidget(
-        seed: friend.userId,
-        radius: 20,
-      ),
-      trailing: Row(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          if (top3.containsKey(position.toString()))
-            Padding(
-              padding: const EdgeInsets.only(right: 8.0),
-              child: TrophyAvatar(
-                trophyType: top3[position.toString()]!,
+    return Container(
+      decoration: friend.userId == "current_user"
+          ? BoxDecoration(
+              border: Border.all(
+                color: Colors.amberAccent,
+                width: 1.5,
               ),
-            ),
-          PopupMenuButton(
-            icon: Icon(LucideIcons.ellipsis_vertical),
-            itemBuilder: (context) {
-              return [
-                PopupMenuItem(
-                  child: ListTile(
-                    leading: Icon(LucideIcons.user),
-                    title: Text('View Profile'),
-                    onTap: () => SnackbarService.showInfoSnackbar(
-                        'View Profile not implemented yet'),
-                  ),
+              borderRadius: BorderRadius.circular(12),
+            )
+          : null,
+      child: ListTile(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+        tileColor: position.isEven
+            ? Theme.of(context).colorScheme.surfaceContainerLow
+            : Theme.of(context).colorScheme.surfaceContainerHigh,
+        leading: AvatarWidget(
+          seed: friend.userId,
+          radius: 20,
+        ),
+        trailing: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            if (top3.containsKey(position.toString()))
+              Padding(
+                padding: const EdgeInsets.only(right: 8.0),
+                child: TrophyAvatar(
+                  trophyType: top3[position.toString()]!,
                 ),
-                PopupMenuItem(
-                  child: ListTile(
-                    leading: Icon(LucideIcons.redo),
-                    title: Text('Nudge'),
-                    onTap: () async {
-                      try {
-                        if (context.mounted) {
-                          Navigator.of(context).pop();
-                        }
-
-                        final response = await FriendRepository()
-                            .nudgeFriend(friend.username);
-
-                        if (context.mounted) {
-                          if (response.success) {
-                            SnackbarService.showSuccessSnackbar(
-                                response.message);
-                          } else {
-                            SnackbarService.showErrorSnackbar(response.message);
-                          }
-                        }
-                      } catch (e) {
-                        if (context.mounted) {
-                          SnackbarService.showErrorSnackbar(e.toString());
-                        }
-                      }
-                    },
-                  ),
-                ),
-                PopupMenuItem(
-                  child: ListTile(
-                    leading: Icon(
-                      LucideIcons.user_minus,
-                      color: Colors.redAccent,
+              ),
+            if (friend.userId != "current_user")
+              PopupMenuButton(
+                icon: Icon(LucideIcons.ellipsis_vertical),
+                itemBuilder: (context) {
+                  return [
+                    PopupMenuItem(
+                      child: ListTile(
+                        leading: Icon(LucideIcons.user),
+                        title: Text('View Profile'),
+                        onTap: () => SnackbarService.showInfoSnackbar(
+                            'View Profile not implemented yet'),
+                      ),
                     ),
-                    title: Text('Remove Friend'),
-                    onTap: () {
-                      onRemove(friend);
-                    },
-                  ),
-                ),
-              ];
-            },
-          ),
-        ],
-      ),
-      title: Text(
-        friend.username,
-        style: Theme.of(context).textTheme.bodyLarge,
-      ),
-      subtitle: Text(
-        '${friend.points} points',
+                    PopupMenuItem(
+                      child: ListTile(
+                        leading: Icon(LucideIcons.redo),
+                        title: Text('Nudge'),
+                        onTap: () async {
+                          try {
+                            if (context.mounted) {
+                              Navigator.of(context).pop();
+                            }
+
+                            final response = await FriendRepository()
+                                .nudgeFriend(friend.username);
+
+                            if (context.mounted) {
+                              if (response.success) {
+                                SnackbarService.showSuccessSnackbar(
+                                    response.message);
+                              } else {
+                                SnackbarService.showErrorSnackbar(
+                                    response.message);
+                              }
+                            }
+                          } catch (e) {
+                            if (context.mounted) {
+                              SnackbarService.showErrorSnackbar(e.toString());
+                            }
+                          }
+                        },
+                      ),
+                    ),
+                    PopupMenuItem(
+                      child: ListTile(
+                        leading: Icon(
+                          LucideIcons.user_minus,
+                          color: Colors.redAccent,
+                        ),
+                        title: Text('Remove Friend'),
+                        onTap: () {
+                          onRemove(friend);
+                        },
+                      ),
+                    ),
+                  ];
+                },
+              ),
+          ],
+        ),
+        title: Text(
+          friend.username,
+          style: Theme.of(context).textTheme.bodyLarge,
+        ),
+        subtitle: Text(
+          '${friend.points} points',
+        ),
       ),
     );
   }
