387ebe3c025a0289fc4d1c49ec9ad6c8576e5424
HayHay <dev@hayhay.dev>
2025-01-08 20:33:53 -0500

    Cleanup

--- lib/settings/views/about/about_screen.dart
+++ lib/settings/views/about/about_screen.dart
@@ -17,197 +17,198 @@ class AboutScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Theme.of(context).scaffoldBackgroundColor,
-      padding: const EdgeInsets.all(20),
-      child: SingleChildScrollView(
-        child: Column(
-          children: [
-            AppBar(
-              surfaceTintColor: Colors.transparent,
-              title: Text(
-                'About',
-                style: Theme.of(context).textTheme.headlineLarge,
-              ),
-            ),
-            Card(
-              child: Padding(
-                padding: const EdgeInsets.all(24.0),
-                child: Column(
-                  children: [
-                    CircleAvatar(
-                      radius: 50,
-                      child: Text(
-                        'QK',
-                        style: Theme.of(context).textTheme.headlineSmall,
-                      ),
-                    ),
-                    const SizedBox(height: 20),
-                    Text(
-                      'QuestKeeper',
-                      style: Theme.of(context).textTheme.headlineSmall,
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            FutureBuilder<PackageInfo>(
-              future: _getPackageInfo(),
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.done) {
-                  if (snapshot.hasError) {
-                    return Center(child: Text('Error: ${snapshot.error}'));
-                  }
-
-                  return Column(
-                    children: [
-                      ListTile(
-                        title: Text(
-                            'v${snapshot.data!.version}+${snapshot.data!.buildNumber}'),
-                      ),
-                    ],
-                  );
-                } else {
-                  return const Center(child: CircularProgressIndicator());
-                }
-              },
-            ),
-            ListTile(
-              title: const Text("Website"),
-              textColor: primaryColor,
-              leading: const Icon(LucideIcons.globe),
-              onTap: () {
-                final uri = Uri.parse("https://questkeeper.app");
-
-                launchUrl(uri);
-              },
-            ),
-            ListTile(
-              title: const Text("View Privacy Policy"),
-              textColor: primaryColor,
-              leading: const Icon(LucideIcons.badge_help),
-              onTap: () {
-                final uri = Uri.parse("https://questkeeper.app/privacy-policy");
-
-                launchUrl(uri);
-              },
-            ),
-            ListTile(
-              title: const Text("View Terms of Service"),
-              textColor: primaryColor,
-              leading: const Icon(LucideIcons.file_check),
-              onTap: () {
-                final uri =
-                    Uri.parse("https://questkeeper.app/terms-of-service");
-
-                launchUrl(uri);
-              },
-            ),
-            ListTile(
-              title: const Text("View licenses"),
-              textColor: secondaryColor,
-              leading: const Icon(LucideIcons.info),
-              onTap: () {
-                showLicensePage(
-                  context: context,
-                );
-              },
-            ),
-            ListTile(
-              leading: Icon(LucideIcons.heart),
-              title: const Text("Special thanks to"),
-              onTap: () => showDialog(
-                context: context,
-                builder: (context) {
-                  return AlertDialog(
-                    title: const Text('Special thanks to'),
-                    content: Column(
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('About'),
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Card(
+                  child: Padding(
+                    padding: const EdgeInsets.all(24.0),
+                    child: Column(
                       children: [
-                        ListTile(
-                          title: const Text('Flero'),
-                          subtitle: const Text('Developer\nTap for website'),
-                          onTap: () => launchUrl(
-                            Uri.parse('https://flero.dev'),
+                        CircleAvatar(
+                          radius: 50,
+                          child: Text(
+                            'QK',
+                            style: Theme.of(context).textTheme.headlineSmall,
                           ),
-                          isThreeLine: true,
                         ),
-                        Divider(),
-                        ListTile(
-                          title: const Text('asifpx_'),
-                          subtitle: const Text(
-                              'Environments and backgrounds\nTap for X'),
-                          onTap: () => launchUrl(
-                            Uri.parse('https://x.com/asifpx_'),
-                          ),
-                          isThreeLine: true,
+                        const SizedBox(height: 20),
+                        Text(
+                          'QuestKeeper',
+                          style: Theme.of(context).textTheme.headlineSmall,
                         ),
-                        ListTile(
-                          title: const Text('Elthen'),
-                          subtitle:
-                              const Text('Animal sprites\nTap for Patreon'),
-                          onTap: () => launchUrl(
-                            Uri.parse('https://www.patreon.com/elthen'),
-                          ),
-                          isThreeLine: true,
-                        ),
-                        ListTile(
-                          title: const Text('vsioneithr'),
-                          subtitle: const Text('Trophy sprites\nTap for X'),
-                          onTap: () => launchUrl(
-                            Uri.parse('https://x.com/vsioneithr'),
-                          ),
-                          isThreeLine: true,
-                        )
                       ],
                     ),
-                    actions: [
-                      TextButton(
-                        onPressed: () {
-                          Navigator.of(context).pop();
-                        },
-                        child: const Text('Close'),
-                      ),
-                    ],
-                  );
-                },
-              ),
-            ),
-            ListTile(
-              leading: Icon(LucideIcons.trash),
-              title: const Text(
-                "Clear local data",
-                style: TextStyle(
-                  color: Colors.redAccent,
+                  ),
                 ),
-              ),
-              onTap: () async {
-                AuthNotifier().clearToken();
-                await SharedPreferences.getInstance().then((prefs) {
-                  prefs.clear();
-                });
+                FutureBuilder<PackageInfo>(
+                  future: _getPackageInfo(),
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.done) {
+                      if (snapshot.hasError) {
+                        return Center(child: Text('Error: ${snapshot.error}'));
+                      }
 
-                // Clear cache manager
-                final CacheManager cacheManager = DefaultCacheManager();
-                await cacheManager.emptyCache();
+                      return Column(
+                        children: [
+                          ListTile(
+                            title: Text(
+                                'v${snapshot.data!.version}+${snapshot.data!.buildNumber}'),
+                          ),
+                        ],
+                      );
+                    } else {
+                      return const Center(child: CircularProgressIndicator());
+                    }
+                  },
+                ),
+                ListTile(
+                  title: const Text("Website"),
+                  textColor: primaryColor,
+                  leading: const Icon(LucideIcons.globe),
+                  onTap: () {
+                    final uri = Uri.parse("https://questkeeper.app");
 
-                if (context.mounted) {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(
-                      content: Text('Local data cleared'),
+                    launchUrl(uri);
+                  },
+                ),
+                ListTile(
+                  title: const Text("View Privacy Policy"),
+                  textColor: primaryColor,
+                  leading: const Icon(LucideIcons.badge_help),
+                  onTap: () {
+                    final uri =
+                        Uri.parse("https://questkeeper.app/privacy-policy");
+
+                    launchUrl(uri);
+                  },
+                ),
+                ListTile(
+                  title: const Text("View Terms of Service"),
+                  textColor: primaryColor,
+                  leading: const Icon(LucideIcons.file_check),
+                  onTap: () {
+                    final uri =
+                        Uri.parse("https://questkeeper.app/terms-of-service");
+
+                    launchUrl(uri);
+                  },
+                ),
+                ListTile(
+                  title: const Text("View licenses"),
+                  textColor: secondaryColor,
+                  leading: const Icon(LucideIcons.info),
+                  onTap: () {
+                    showLicensePage(
+                      context: context,
+                    );
+                  },
+                ),
+                ListTile(
+                  leading: Icon(LucideIcons.heart),
+                  title: const Text("Special thanks to"),
+                  onTap: () => showDialog(
+                    context: context,
+                    builder: (context) {
+                      return AlertDialog(
+                        title: const Text('Special thanks to'),
+                        content: Column(
+                          children: [
+                            ListTile(
+                              title: const Text('Flero'),
+                              subtitle:
+                                  const Text('Developer\nTap for website'),
+                              onTap: () => launchUrl(
+                                Uri.parse('https://flero.dev'),
+                              ),
+                              isThreeLine: true,
+                            ),
+                            Divider(),
+                            ListTile(
+                              title: const Text('asifpx_'),
+                              subtitle: const Text(
+                                  'Environments and backgrounds\nTap for X'),
+                              onTap: () => launchUrl(
+                                Uri.parse('https://x.com/asifpx_'),
+                              ),
+                              isThreeLine: true,
+                            ),
+                            ListTile(
+                              title: const Text('Elthen'),
+                              subtitle:
+                                  const Text('Animal sprites\nTap for Patreon'),
+                              onTap: () => launchUrl(
+                                Uri.parse('https://www.patreon.com/elthen'),
+                              ),
+                              isThreeLine: true,
+                            ),
+                            ListTile(
+                              title: const Text('vsioneithr'),
+                              subtitle: const Text('Trophy sprites\nTap for X'),
+                              onTap: () => launchUrl(
+                                Uri.parse('https://x.com/vsioneithr'),
+                              ),
+                              isThreeLine: true,
+                            )
+                          ],
+                        ),
+                        actions: [
+                          TextButton(
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                            child: const Text('Close'),
+                          ),
+                        ],
+                      );
+                    },
+                  ),
+                ),
+                ListTile(
+                  leading: Icon(LucideIcons.trash),
+                  title: const Text(
+                    "Clear local data",
+                    style: TextStyle(
+                      color: Colors.redAccent,
                     ),
-                  );
-                  Navigator.of(context).pushNamedAndRemoveUntil(
-                    '/signin',
-                    (route) => false,
-                  );
-                }
-              },
+                  ),
+                  onTap: () async {
+                    AuthNotifier().clearToken();
+                    await SharedPreferences.getInstance().then((prefs) {
+                      prefs.clear();
+                    });
+
+                    // Clear cache manager
+                    final CacheManager cacheManager = DefaultCacheManager();
+                    await cacheManager.emptyCache();
+
+                    if (context.mounted) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                          content: Text('Local data cleared'),
+                        ),
+                      );
+                      Navigator.of(context).pushNamedAndRemoveUntil(
+                        '/signin',
+                        (route) => false,
+                      );
+                    }
+                  },
+                ),
+                const ListTile(
+                  title: Text('© 2024 Ishan Misra'),
+                  subtitle: Text('All rights reserved'),
+                ),
+              ],
             ),
-            const ListTile(
-              title: Text('© 2024 Ishan Misra'),
-              subtitle: Text('All rights reserved'),
-            ),
-          ],
+          ),
         ),
       ),
     );
