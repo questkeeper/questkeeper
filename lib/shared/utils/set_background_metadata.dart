6ef95d05a5986051e006097a5153ee0688be2d6f
HayHay <dev@hayhay.dev>
2025-01-15 11:44:30 -0500

    Add safeAreaMigration

--- lib/shared/utils/set_background_metadata.dart
+++ lib/shared/utils/set_background_metadata.dart
@@ -6,11 +6,17 @@ import 'package:questkeeper/shared/utils/shared_preferences_manager.dart';
 import 'package:questkeeper/shared/extensions/datetime_extensions.dart';
 
 /// Sets the background metadata for the space BGs in Shared Preferences on launch
+@Deprecated("This is a notice to remove safeAreaMigration down the line")
 void setBackgroundMetadata() async {
   final prefs = SharedPreferencesManager.instance;
   final metadataPath = "assets/images/backgrounds/metadata.json";
 
-  if (prefs.getBool("backgroundsIsSet") ?? false) return;
+  // This is to check if the top stuff has been cached pre-0.8.4+1
+  final safeAreaMigration = prefs.getBool("safeAreaMigration") ?? false;
+
+  final backgroundIsSet = prefs.getBool("backgroundsIsSet") ?? false;
+
+  if (backgroundIsSet && safeAreaMigration) return;
 
   final metadataJson = json.decode(
     await rootBundle.loadString(metadataPath),
