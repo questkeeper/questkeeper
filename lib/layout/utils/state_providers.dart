3a45ce2a299f65269a57d47b9fcda9d9fd57bb32
HayHay <dev@hayhay.dev>
2025-02-06 19:04:30 -0500

    Context pane state provider

--- lib/layout/utils/state_providers.dart
+++ lib/layout/utils/state_providers.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final navRailExpandedProvider = StateProvider<bool>((ref) => false);
 final zenModeProvider = StateProvider<bool>((ref) => false);
 final commandPaletteVisibleProvider = StateProvider<bool>((ref) => false);
+final contextPaneProvider = StateProvider<Widget?>((ref) => null);
