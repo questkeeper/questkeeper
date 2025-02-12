72191ad4a9ebfa0487eb6bf889209c7b6feac397
HayHay <dev@hayhay.dev>
2025-02-12 15:24:30 -0500

    Debug settings changes

--- lib/settings/views/debug/debug_settings.dart
+++ lib/settings/views/debug/debug_settings.dart
@@ -1,6 +1,10 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:numberpicker/numberpicker.dart';
 import 'package:questkeeper/settings/widgets/settings_card.dart';
+import 'package:questkeeper/shared/extensions/datetime_extensions.dart';
 import 'package:questkeeper/shared/notifications/local_notification_service.dart';
 import 'package:questkeeper/shared/utils/shared_preferences_manager.dart';
 import 'package:questkeeper/shared/widgets/show_drawer.dart';
@@ -230,6 +234,56 @@ class SuperSecretDebugSettings extends StatelessWidget {
             },
             description:
                 "Clears the onboarding overlay once logged in, resets the overlay to show again",
+          ),
+          _distinctListTiles(
+            key: "backgroundsIsSet",
+            title: "Clear the backgroundsIsSet flag",
+            onTap: () async => await prefs.remove('backgroundsIsSet'),
+            description: "Clears the backgroundsIsSet flag",
+          ),
+          _distinctListTiles(
+            key: "allBackgrounds",
+            title: "Clear all backgrounds",
+            onTap: () async {
+              final metadataPath = "assets/images/backgrounds/metadata.json";
+              final metadataString = await rootBundle.loadString(metadataPath);
+              if (metadataString.isEmpty) {
+                debugPrint('Error: Background metadata file is empty');
+                return;
+              }
+
+              final metadataJson = json.decode(metadataString);
+              if (metadataJson == null ||
+                  !metadataJson.containsKey("backgroundTypes")) {
+                debugPrint('Error: Invalid background metadata format');
+                return;
+              }
+
+              await Future.wait<void>(
+                metadataJson["backgroundTypes"].map<Future<void>>((bg) async {
+                  try {
+                    for (var i = 0; i < 3; i++) {
+                      final timeOfDay = TimeOfDayType.values[i];
+                      final timeOfDayStr = timeOfDay.toString().split('.').last;
+
+                      // Set main background color
+                      await prefs.remove(
+                        "background_${bg["name"]}_$timeOfDayStr",
+                      );
+
+                      // Set top background color
+                      await prefs.remove(
+                        "background_${bg["name"]}_top_$timeOfDayStr",
+                      );
+                    }
+                  } catch (e) {
+                    debugPrint(
+                        'Error setting background for ${bg["name"]}: $e');
+                  }
+                }).toList(), // Convert Iterable to List
+              );
+            },
+            description: "Clears all backgrounds",
           ),
         ],
       ),
