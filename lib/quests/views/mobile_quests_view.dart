9d3fcad0d1e265727e7f3649794f50594ae82b37
HayHay <dev@hayhay.dev>
2025-04-13 20:09:18 -0400

    minor ui fixes

--- lib/quests/views/mobile_quests_view.dart
+++ lib/quests/views/mobile_quests_view.dart
@@ -36,6 +36,7 @@ class MobileQuestsView extends ConsumerWidget {
                 achievements,
                 badgesAsync.isLoading,
               ),
+              const SizedBox(height: 96),
             ],
           ),
         ),
